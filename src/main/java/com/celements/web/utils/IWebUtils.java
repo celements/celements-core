83a3784b456f94abf0a602c0bef971cd5a207405
ebeutler <edoardo.beutler@synventis.com>
2012-06-22 15:08:40 +0200

    added Api method to get attachment list ordered

--- src/main/java/com/celements/web/utils/IWebUtils.java
+++ src/main/java/com/celements/web/utils/IWebUtils.java
@@ -204,6 +204,9 @@ public interface IWebUtils {
   public List<Attachment> getAttachmentListSorted(Document doc, String comparator
       ) throws ClassNotFoundException;
 
+  public List<Attachment> getAttachmentListSorted(Document doc, String comparator,
+      boolean imagesOnly, int start, int nb) throws ClassNotFoundException;
+
   public String getAttachmentListSortedAsJSON(Document doc, String comparator,
       boolean imagesOnly);
   
