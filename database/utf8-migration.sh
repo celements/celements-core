cb4984317864b04de82e67c9864d35fb4a475528
Fabian Pichler <fabian.pichler@synventis.com>
2012-08-31 19:20:21 +0200

    remove utf8_bin collation fix

--- database/utf8-migration.sh
+++ database/utf8-migration.sh
@@ -11,11 +11,6 @@ echo "show tables \G" | mysql -u root --password="$password" "$database" \
   echo "migrating table $tablename ..."
   echo "ALTER TABLE \`$tablename\` CONVERT TO CHARACTER SET utf8 COLLATE utf8_bin;" \
   | mysql -u root --password="$password" "$database"
-  echo "alter table xwikidoc modify column XWD_FULLNAME varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL; \
-alter table xwikidoc modify column XWD_NAME varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL; \
-alter table xwikidoc modify column XWD_WEB varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL; \
-alter table xwikiobjects modify column XWO_NAME varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL;" \
-| mysql -u root --password="$password" "$database"
 done
 
 echo "Migration finished."
