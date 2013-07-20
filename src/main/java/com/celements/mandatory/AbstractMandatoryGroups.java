d1858afacb4f792ddbd03b542fa57a9ac5da6dc8
Fabian Pichler <fabian.pichler@synventis.com>
2013-07-20 10:25:47 +0200

    add mandatory CelementsGroups add AbstractMandatoryGroups class

---
+++ src/main/java/com/celements/mandatory/AbstractMandatoryGroups.java
@@ -0,0 +1,44 @@
+package com.celements.mandatory;
+
+import org.xwiki.component.annotation.Requirement;
+import org.xwiki.context.Execution;
+import org.xwiki.model.reference.DocumentReference;
+
+import com.celements.web.plugin.cmd.CreateDocumentCommand;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiDocument;
+
+public abstract class AbstractMandatoryGroups implements IMandatoryDocumentRole {
+
+  @Requirement
+  Execution execution;
+
+  public abstract void checkDocuments() throws XWikiException;
+
+  public AbstractMandatoryGroups() {
+    super();
+  }
+
+  protected XWikiContext getContext() {
+    return (XWikiContext)execution.getContext().getProperty("xwikicontext");
+  }
+
+  protected void checkGroup(DocumentReference groupRef) throws XWikiException {
+    if (!getContext().getWiki().exists(groupRef, getContext())) {
+      XWikiDocument editorGroupDoc = new CreateDocumentCommand().createDocument(groupRef,
+          "UserGroup");
+      if (editorGroupDoc != null) {
+        editorGroupDoc.newXObject(getGroupClassRef(getContext().getDatabase()),
+            getContext());
+        getContext().getWiki().saveDocument(editorGroupDoc, "autocreate mandatory"
+            + " celements group.", getContext());
+      }
+    }
+  }
+
+  protected DocumentReference getGroupClassRef(String wikiName) {
+    return new DocumentReference(wikiName, "XWiki", "XWikiGroups");
+  }
+
+}
---
+++ src/main/java/com/celements/mandatory/CelementsGroups.java
@@ -0,0 +1,30 @@
+package com.celements.mandatory;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.DocumentReference;
+
+import com.xpn.xwiki.XWikiException;
+
+@Component("celements.MandatoryGroups")
+public class CelementsGroups extends AbstractMandatoryGroups {
+
+  @Override
+  public void checkDocuments() throws XWikiException {
+    checkGroup(getContentEditorGroupRef(getContext().getDatabase()));
+    checkGroup(getAdminGroupRef(getContext().getDatabase()));
+    checkGroup(getAllGroupRef(getContext().getDatabase()));
+  }
+
+  public DocumentReference getAdminGroupRef(String wikiName) {
+    return new DocumentReference(wikiName, "XWiki", "XWikiAdminGroup");
+  }
+
+  public DocumentReference getAllGroupRef(String wikiName) {
+    return new DocumentReference(wikiName, "XWiki", "XWikiAllGroup");
+  }
+
+  public DocumentReference getContentEditorGroupRef(String wikiName) {
+    return new DocumentReference(wikiName, "XWiki", "ContentEditorsGroup");
+  }
+
+}
