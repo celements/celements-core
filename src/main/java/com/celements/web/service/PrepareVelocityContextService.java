a64e0522b437349921a518d51b0676e0242512a0
Fabian Pichler <fabian.pichler@synventis.com>
2014-01-27 10:42:01 +0100

    fix NPE in getPageTypeDoc in PrepareVelocityContextService

--- src/main/java/com/celements/web/service/PrepareVelocityContextService.java
+++ src/main/java/com/celements/web/service/PrepareVelocityContextService.java
@@ -331,12 +331,18 @@ public class PrepareVelocityContextService implements IPrepareVelocityContext {
   private XWikiDocument getPageTypeDoc(XWikiContext context) {
     if(context.getDoc() != null) {
       try {
-        XWikiDocument pageTypeDoc = new PageType(new DocumentReference(
-            context.getDatabase(), "PageTypes",
-            pageTypeResolver.getPageTypeRefForCurrentDoc().getConfigName())
-            ).getTemplateDocument(getContext());
-        LOGGER.debug("getPageTypeDoc: pageTypeDoc=" + pageTypeDoc);
-        return pageTypeDoc;
+        PageTypeReference pTRefForCurrDoc = pageTypeResolver.getPageTypeRefForCurrentDoc(
+            );
+        if (pTRefForCurrDoc != null) {
+          DocumentReference pageTypeDocRef = new DocumentReference(context.getDatabase(),
+              "PageTypes", pTRefForCurrDoc.getConfigName());
+          XWikiDocument pageTypeDoc = new PageType(pageTypeDocRef).getTemplateDocument(
+              getContext());
+          LOGGER.debug("getPageTypeDoc: pageTypeDoc=" + pageTypeDoc);
+          return pageTypeDoc;
+        } else {
+          LOGGER.info("no pageType reference for current document found.");
+        }
       } catch (XWikiException exp) {
         LOGGER.error("Failed to getPageTypeDoc.", exp);
       }
