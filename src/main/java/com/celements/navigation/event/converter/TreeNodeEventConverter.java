78d3f8265de621815d4246ef725239419aff4fd9
Fabian Pichler <fabian.pichler@synventis.com>
2014-05-04 09:26:14 +0200

    fix TreeNodeEventConverter with replacement from DocumentEventConverter

--- src/main/java/com/celements/navigation/event/converter/TreeNodeEventConverter.java
+++ src/main/java/com/celements/navigation/event/converter/TreeNodeEventConverter.java
@@ -1,6 +1,8 @@
 package com.celements.navigation.event.converter;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -8,21 +10,52 @@ import org.xwiki.component.annotation.Component;
 import org.xwiki.observation.event.Event;
 import org.xwiki.observation.remote.LocalEventData;
 import org.xwiki.observation.remote.RemoteEventData;
-import org.xwiki.observation.remote.converter.AbstractEventConverter;
 
-@Component("TreeNodeEventConverter")
-public class TreeNodeEventConverter extends AbstractEventConverter {
+import com.celements.navigation.event.TreeNodeCreatedEvent;
+import com.celements.navigation.event.TreeNodeDeletedEvent;
+import com.celements.navigation.event.TreeNodeUpdatedEvent;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.internal.observation.remote.converter.AbstractXWikiEventConverter;
+
+@Component("TreeNode")
+public class TreeNodeEventConverter extends AbstractXWikiEventConverter {
 
   private static Log LOGGER = LogFactory.getFactory().getInstance(
       TreeNodeEventConverter.class);
 
   /**
+   * The events supported by this converter.
+   */
+  private Set<Class<? extends Event>> events = new HashSet<Class<? extends Event>>() {
+    private static final long serialVersionUID = 1L;
+    {
+      add(TreeNodeDeletedEvent.class);
+      add(TreeNodeCreatedEvent.class);
+      add(TreeNodeUpdatedEvent.class);
+    }
+  };
+
+  /**
    * {@inheritDoc}
    * 
-   * @see org.xwiki.observation.remote.converter.AbstractEventConverter#getPriority()
+   * @see org.xwiki.observation.remote.converter.LocalEventConverter#toRemote(org.xwiki.observation.remote.LocalEventData,
+   *      org.xwiki.observation.remote.RemoteEventData)
    */
-  public int getPriority() {
-    return 1000;
+  public boolean toRemote(LocalEventData localEvent, RemoteEventData remoteEvent) {
+    if (this.events.contains(localEvent.getEvent().getClass())) {
+      LOGGER.trace("toRemote: serialize event [" + localEvent.getEvent().getClass()
+          + "].");
+      // fill the remote event
+      remoteEvent.setEvent((Serializable) localEvent.getEvent());
+      remoteEvent.setSource(serializeXWikiDocument(
+          (XWikiDocument) localEvent.getSource()));
+      remoteEvent.setData(serializeXWikiContext((XWikiContext) localEvent.getData()));
+      return true;
+    } else {
+      LOGGER.debug("toRemote: skip event [" + localEvent.getEvent().getClass() + "].");
+    }
+    return false;
   }
 
   /**
@@ -32,71 +65,21 @@ public class TreeNodeEventConverter extends AbstractEventConverter {
    *      org.xwiki.observation.remote.LocalEventData)
    */
   public boolean fromRemote(RemoteEventData remoteEvent, LocalEventData localEvent) {
-    LOGGER.debug("fromRemote remoteEvent [" + remoteEvent + "], localEvent [" + localEvent
-        + "].");
-    if (remoteEvent.getEvent() instanceof Event) {
-      localEvent.setEvent((Event) remoteEvent.getEvent());
-      if (remoteEvent.getSource() != null) {
-        localEvent.setSource(remoteEvent.getSource());
-      }
-      if (remoteEvent.getData() != null) {
-        localEvent.setData(remoteEvent.getData());
+    if (this.events.contains(remoteEvent.getEvent().getClass())) {
+      LOGGER.trace("fromRemote: serialize event [" + localEvent.getEvent().getClass()
+          + "].");
+      // fill the local event
+      XWikiContext context = unserializeXWikiContext(remoteEvent.getData());
+      if (context != null) {
+        localEvent.setEvent((Event) remoteEvent.getEvent());
+        localEvent.setSource(unserializeDocument(remoteEvent.getSource()));
+        localEvent.setData(unserializeXWikiContext(remoteEvent.getData()));
       }
       return true;
     } else {
-      LOGGER.debug("fromRemote not instance of Event [" + remoteEvent.getEvent() + "].");
+      LOGGER.debug("fromRemote: skip event [" + localEvent.getEvent().getClass() + "].");
     }
     return false;
-  }
-
-  /**
-   * {@inheritDoc}
-   * 
-   * @see org.xwiki.observation.remote.converter.LocalEventConverter#toRemote(org.xwiki.observation.remote.LocalEventData,
-   *      org.xwiki.observation.remote.RemoteEventData)
-   */
-  public boolean toRemote(LocalEventData localEvent, RemoteEventData remoteEvent) {
-    if (isSerializable(localEvent)) {
-      remoteEvent.setEvent((Serializable) localEvent.getEvent());
-      remoteEvent.setSource((Serializable) localEvent.getSource());
-      remoteEvent.setData((Serializable) localEvent.getData());
-      LOGGER.debug("toRemote serializable [" + localEvent.getClass().getName()
-          + "], remoteEvent [" + remoteEvent + "], localEvent [" + localEvent + "].");
-      return true;
-    } else {
-      LOGGER.debug("toRemote not serializable [" + localEvent.getClass().getName()
-          + "], localEvent [" + localEvent + "].");
-    }
-    return false;
-  }
-
-  /**
-   * Indicate if a local event is fully serializable.
-   * 
-   * @param localEvent
-   *          the local event
-   * @return true is the local event is fully serializable, false otherwise.
-   */
-  private boolean isSerializable(LocalEventData localEvent) {
-    boolean isSerializable = localEvent.getEvent() instanceof Serializable
-        && isSerializable(localEvent.getData()) && isSerializable(localEvent.getSource());
-    LOGGER.trace("isSerializable: [" + isSerializable + "], getEvent instanceof ["
-        + (localEvent.getEvent() instanceof Serializable) + "] isSerializable getData ["
-        + isSerializable(localEvent.getData()) + "] isSerializable getSource ["
-        + isSerializable(localEvent.getSource()) + "] source class ["
-        + localEvent.getSource().getClass().getName() + "].");
-    return isSerializable;
-  }
-
-  /**
-   * Indicate if an object is serializable.
-   * 
-   * @param obj
-   *          the object to test
-   * @return true is the object is serializable, false otherwise.
-   */
-  private boolean isSerializable(Object obj) {
-    return obj instanceof Serializable || obj == null;
   }
 
 }
