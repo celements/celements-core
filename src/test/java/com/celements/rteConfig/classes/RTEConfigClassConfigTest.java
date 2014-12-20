2c3a53965806216e27537c6934f8d954acdb4006
Fabian Pichler <fabian.pichler@synventis.com>
2014-12-20 14:08:59 +0100

    add IRTEConfigClassConfig move class creation from oldCoreClasses to RTEConfigClasses change rte_styles field to TextArea

---
+++ src/test/java/com/celements/rteConfig/classes/RTEConfigClassConfigTest.java
@@ -0,0 +1,53 @@
+package com.celements.rteConfig.classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.EntityReference;
+import org.xwiki.model.reference.WikiReference;
+
+import com.celements.common.test.AbstractBridgedComponentTestCase;
+import com.xpn.xwiki.web.Utils;
+
+public class RTEConfigClassConfigTest extends AbstractBridgedComponentTestCase {
+
+  private RTEConfigClassConfig rteConfigClassCfg;
+
+  @Before
+  public void setUp_RTEConfigClassConfigTest() throws Exception {
+    rteConfigClassCfg = (RTEConfigClassConfig) Utils.getComponent(
+        IRTEConfigClassConfig.class);
+  }
+
+  @Test
+  public void testGetRTEConfigTypePropertiesClassRefEntityReference() {
+    EntityReference inRef = new EntityReference("xwikiDb", EntityType.WIKI);
+    replayDefault();
+    DocumentReference classRef = rteConfigClassCfg.getRTEConfigTypePropertiesClassRef(
+        inRef);
+    assertEquals("xwikiDb", classRef.extractReference(EntityType.WIKI).getName());
+    assertEquals(IRTEConfigClassConfig.RTE_CONFIG_TYPE_PRPOP_CLASS_SPACE,
+        classRef.extractReference(EntityType.SPACE).getName());
+    assertEquals(IRTEConfigClassConfig.RTE_CONFIG_TYPE_PRPOP_CLASS_DOC,
+        classRef.getName());
+    verifyDefault();
+  }
+
+  @Test
+  public void testGetRTEConfigTypePropertiesClassRefWikiReference() {
+    WikiReference inRef = new WikiReference("xwikiDb");
+    replayDefault();
+    DocumentReference classRef = rteConfigClassCfg.getRTEConfigTypePropertiesClassRef(
+        inRef);
+    assertEquals("xwikiDb", classRef.extractReference(EntityType.WIKI).getName());
+    assertEquals(IRTEConfigClassConfig.RTE_CONFIG_TYPE_PRPOP_CLASS_SPACE,
+        classRef.extractReference(EntityType.SPACE).getName());
+    assertEquals(IRTEConfigClassConfig.RTE_CONFIG_TYPE_PRPOP_CLASS_DOC,
+        classRef.getName());
+    verifyDefault();
+  }
+
+}
