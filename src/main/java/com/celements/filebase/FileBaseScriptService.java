d7297513840fcd2f246f1ad08d9590ab5d699c58
ebeutler <edoardo.beutler@synventis.com>
2016-02-17 11:22:39 +0100

    add several FileBaseService methods to script service

--- src/main/java/com/celements/filebase/FileBaseScriptService.java
+++ src/main/java/com/celements/filebase/FileBaseScriptService.java
@@ -1,5 +1,6 @@
 package com.celements.filebase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -11,9 +12,16 @@ import org.xwiki.model.reference.AttachmentReference;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.script.service.ScriptService;
 
+import com.celements.filebase.exceptions.FileBaseLoadException;
+import com.celements.filebase.exceptions.FileNotExistsException;
+import com.celements.filebase.matcher.IAttachmentMatcher;
 import com.celements.web.plugin.cmd.TokenBasedUploadCommand;
 import com.celements.web.service.IWebUtilsService;
+import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.api.Attachment;
+import com.xpn.xwiki.api.Document;
+import com.xpn.xwiki.doc.XWikiAttachment;
 
 @Component("filebase")
 public class FileBaseScriptService implements ScriptService {
@@ -28,6 +36,9 @@ public class FileBaseScriptService implements ScriptService {
 
   @Requirement
   IAttachmentServiceRole attachmentService;
+
+  @Requirement(SingleDocFileBaseService.FILEBASE_SINGLE_DOC)
+  IFileBaseServiceRole filebaseService;
 
   public String clearFileName(String fileName) {
     return attachmentService.clearFileName(fileName);
@@ -51,5 +62,35 @@ public class FileBaseScriptService implements ScriptService {
   
   public int deleteAttachmentList(List<AttachmentReference> attachmentRefList) {
     return attachmentService.deleteAttachmentList(attachmentRefList);
+  }
+
+  public boolean existsFileNameEqual(String filename) throws FileBaseLoadException {
+    return filebaseService.existsFileNameEqual(filename);
+  }
+
+  public Attachment getFileNameEqual(String filename) throws FileBaseLoadException {
+    try {
+      XWikiAttachment xwikiAtt = filebaseService.getFileNameEqual(filename);
+      Document fbDoc = xwikiAtt.getDoc().newDocument(getContext());
+      return new Attachment(fbDoc, xwikiAtt, getContext());
+    } catch (FileNotExistsException e) {
+      _LOGGER.trace("Filebase could not find file [" + filename + "]");
+      return null;
+    }
+  }
+
+  public List<Attachment> getFilesNameMatch(IAttachmentMatcher attMatcher
+      ) throws FileBaseLoadException {
+    List<XWikiAttachment> xwikiAttList = filebaseService.getFilesNameMatch(attMatcher);
+    List<Attachment> attList = new ArrayList<Attachment>();
+    for(XWikiAttachment xwikiAtt : xwikiAttList) {
+      Document fbDoc = xwikiAtt.getDoc().newDocument(getContext());
+      attList.add(new Attachment(fbDoc, xwikiAtt, getContext()));
+    }
+    return attList;
+  }
+
+  private XWikiContext getContext() {
+    return (XWikiContext)execution.getContext().getProperty("xwikicontext");
   }
 }
