4526d29d5115395242f567fe7099e11f15c1dd86
Fabian Pichler <fabian.pichler@synventis.com>
2016-02-18 14:55:43 +0100

    change NoAccessRightsException to extend Exception instead of DocumentAccessException

--- src/main/java/com/celements/model/access/exception/NoAccessRightsException.java
+++ src/main/java/com/celements/model/access/exception/NoAccessRightsException.java
@@ -1,30 +1,38 @@
 package com.celements.model.access.exception;
 
-import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.EntityReference;
 
 import com.celements.rights.AccessLevel;
 import com.xpn.xwiki.user.api.XWikiUser;
 
-public class NoAccessRightsException extends DocumentAccessException {
+public class NoAccessRightsException extends Exception {
 
   private static final long serialVersionUID = -2592241907630457229L;
+
+  private EntityReference entityRef;
 
   private XWikiUser user;
   
   private AccessLevel expectedAccessLevel;
 
-  public NoAccessRightsException(DocumentReference docRef, XWikiUser user,
+  public NoAccessRightsException(EntityReference entityRef, XWikiUser user,
       AccessLevel expectedAccessLevel) {
-    super(docRef);
+    super();
+    this.entityRef = entityRef;
     this.user = user;
     this.expectedAccessLevel = expectedAccessLevel;
   }
 
-  public NoAccessRightsException(DocumentReference docRef, XWikiUser user,
+  public NoAccessRightsException(EntityReference entityRef, XWikiUser user,
       AccessLevel expectedAccessLevel, Throwable cause) {
-    super(docRef, cause);
+    super(cause);
+    this.entityRef = entityRef;
     this.user = user;
     this.expectedAccessLevel = expectedAccessLevel;
+  }
+  
+  public EntityReference getEntityRef() {
+    return entityRef;
   }
 
   public XWikiUser getUser() {
