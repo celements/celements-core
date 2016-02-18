f2d0b059ba3f90c6644ea6a4ecabcc4036d45384
Fabian Pichler <fabian.pichler@synventis.com>
2016-02-18 19:15:06 +0100

    move hasAccessLevel api method to RightsAccesScriptService

--- src/main/java/com/celements/rights/CelementsRightScriptService.java
+++ src/main/java/com/celements/rights/CelementsRightScriptService.java
@@ -8,49 +8,71 @@ import org.xwiki.context.Execution;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.script.service.ScriptService;
 
-import com.celements.rights.CelementsRightServiceImpl.PubUnpub;
+import com.celements.rights.access.RightsAccessScriptService;
+import com.celements.rights.publication.EPubUnpub;
 import com.celements.web.service.IWebUtilsService;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.user.api.XWikiRightService;
 import com.xpn.xwiki.web.Utils;
 
+/**
+ * @deprecated use RightsAccessScriptService instead
+ */
 @Component("celementsright")
+@Deprecated
 public class CelementsRightScriptService implements ScriptService {
   
   private static Log LOGGER = LogFactory.getFactory().getInstance(
       CelementsRightScriptService.class);
   
+  @Requirement(RightsAccessScriptService.NAME)
+  ScriptService rightsAccess;
+
   @Requirement
   Execution execution;
-  
+
+  private RightsAccessScriptService getRightsAccess() {
+    return (RightsAccessScriptService) rightsAccess;
+  }
+
+  /**
+   * @deprecated use RightsAccessScriptService.isPublishActive instead
+   */
+  @Deprecated
   public boolean publicationActivated(DocumentReference forDoc) {
-    XWikiRightService rightService = getContext().getWiki().getRightService();
-    if(rightService instanceof CelementsRightServiceImpl) {
-      return ((CelementsRightServiceImpl)rightService).isPublishActive(forDoc, 
-          getContext());
-    } else {
-      LOGGER.warn("Needs CelementsRightServiceImpl for publish / unpublish to work");
-      return false;
-    }
+    return getRightsAccess().isPublishActive(forDoc);
   }
   
+  /**
+   * @deprecated use RightsAccessScriptService.hasAccessLevel instead
+   */
+  @Deprecated
   public boolean hasAccessLevelPublished(String right, String username, 
       DocumentReference docname) {
     return hasAccessLevel(right, username, getWebUtils().getRefLocalSerializer(
-        ).serialize(docname), CelementsRightServiceImpl.PubUnpub.PUBLISHED);
+        ).serialize(docname), EPubUnpub.PUBLISHED);
   }
   
+  /**
+   * @deprecated use RightsAccessScriptService.hasAccessLevel instead
+   */
+  @Deprecated
   public boolean hasAccessLevelUnpublished(String right, String username, 
       DocumentReference docname) {
     return hasAccessLevel(right, username, getWebUtils().getRefLocalSerializer(
-        ).serialize(docname), CelementsRightServiceImpl.PubUnpub.UNPUBLISHED);
+        ).serialize(docname), EPubUnpub.UNPUBLISHED);
   }
   
+  /**
+   * @deprecated use RightsAccessScriptService.hasAccessLevel instead
+   */
+  @Deprecated
   boolean hasAccessLevel(String right, String username, String docname, 
-      PubUnpub unpublished) {
+      EPubUnpub unpublished) {
     XWikiRightService rightService = getContext().getWiki().getRightService();
     if(rightService instanceof CelementsRightServiceImpl) {
+      //TODO replace with overridePubUnpub on PublicationService
       getContext().put("overridePubCheck", unpublished);
     } else {
       LOGGER.warn("Needs CelementsRightServiceImpl for publish / unpublish to work");
