e5cf8f040aace850547f55f42c0dc645323cc43d
Marc Sladek <marc.sladek@synventis.com>
2016-04-04 15:59:26 +0200

    LGPL license added

--- src/main/java/com/celements/configuration/CelementsAllConfigurationSource.java
+++ src/main/java/com/celements/configuration/CelementsAllConfigurationSource.java
@@ -1,3 +1,22 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package com.celements.configuration;
 
 import org.xwiki.component.annotation.Component;
@@ -39,7 +58,7 @@ public class CelementsAllConfigurationSource extends CompositeConfigurationSourc
 
   @Override
   public void initialize() throws InitializationException {
-    // First source is looked first when a property value is requested.
+    // first source is looked up first when a property value is requested.
     addConfigurationSource(this.userPreferencesSource);
     addConfigurationSource(this.spacePreferencesSource);
     addConfigurationSource(this.wikiPreferencesSource);
