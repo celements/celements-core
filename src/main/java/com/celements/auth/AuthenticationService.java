3307800268f46f7a43405589a2fe1125a335251a
Fabian Pichler <fabian.pichler@synventis.com>
2016-06-01 19:37:16 +0200

    format source

--- src/main/java/com/celements/auth/AuthenticationService.java
+++ src/main/java/com/celements/auth/AuthenticationService.java
@@ -27,8 +27,8 @@ import com.xpn.xwiki.user.impl.xwiki.XWikiRightServiceImpl;
 @Component
 public class AuthenticationService implements IAuthenticationServiceRole {
 
-  private static Logger _LOGGER  = LoggerFactory.getLogger(AuthenticationService.class);
-  
+  private static Logger _LOGGER = LoggerFactory.getLogger(AuthenticationService.class);
+
   @Requirement
   private IWebUtilsService webUtilsService;
 
@@ -39,17 +39,17 @@ public class AuthenticationService implements IAuthenticationServiceRole {
   private Execution execution;
 
   private XWikiContext getContext() {
-    return (XWikiContext)execution.getContext().getProperty("xwikicontext");
+    return (XWikiContext) execution.getContext().getProperty("xwikicontext");
   }
-  
-  @Override 
+
+  @Override
   public String getPasswordHash(String encoding, String str) {
     return new PasswordClass().getEquivalentPassword(encoding, str);
   }
-  
-  @Override 
-  public Map<String, String> activateAccount(String activationCode
-      ) throws AccountActivationFailedException {
+
+  @Override
+  public Map<String, String> activateAccount(String activationCode)
+      throws AccountActivationFailedException {
     _LOGGER.debug("activateAccount: for code " + activationCode);
     try {
       Map<String, String> userAccount = new HashMap<String, String>();
@@ -57,27 +57,26 @@ public class AuthenticationService implements IAuthenticationServiceRole {
       String username = new UserNameForUserDataCommand().getUsernameForUserData(hashedCode,
           "validkey", getContext());
       _LOGGER.debug("activateAccount: username = " + username);
-      
-      if((username != null) && !username.equals("")){
+
+      if ((username != null) && !username.equals("")) {
         String password = getContext().getWiki().generateRandomString(24);
-  
+
         DocumentReference userDocRef = webUtilsService.resolveDocumentReference(username);
         _LOGGER.debug("activateAccount: userDocRef = " + userDocRef);
         XWikiDocument doc = modelAccess.getDocument(userDocRef);
         _LOGGER.debug("activateAccount: userDoc = " + doc);
-        DocumentReference userObjRef = webUtilsService.resolveDocumentReference(
-            "XWiki.XWikiUsers");
+        DocumentReference userObjRef = webUtilsService.resolveDocumentReference("XWiki.XWikiUsers");
         _LOGGER.debug("activateAccount: userObjRef = " + userObjRef);
         BaseObject obj = doc.getXObject(userObjRef);
         _LOGGER.debug("activateAccount: userObj = " + obj);
-  
-  //      obj.set("validkey", "", context);
+
+        // obj.set("validkey", "", context);
         obj.set("active", "1", getContext());
         obj.set("force_pwd_change", "1", getContext());
         obj.set("password", password, getContext());
-        
+
         modelAccess.saveDocument(doc, "activate account");
-        
+
         userAccount.put("username", username);
         userAccount.put("password", password);
       }
@@ -87,37 +86,39 @@ public class AuthenticationService implements IAuthenticationServiceRole {
       throw new AccountActivationFailedException(exp);
     }
   }
-  
-  @Override 
-  public XWikiUser checkAuth(String logincredential, String password,
-      String rememberme, String possibleLogins, Boolean noRedirect) 
-          throws XWikiException {
-    String loginname = new UserNameForUserDataCommand().getUsernameForUserData(
-        logincredential, possibleLogins, getContext());
+
+  @Override
+  public XWikiUser checkAuth(String logincredential, String password, String rememberme,
+      String possibleLogins, Boolean noRedirect) throws XWikiException {
+    String loginname = new UserNameForUserDataCommand().getUsernameForUserData(logincredential,
+        possibleLogins, getContext());
     if ("".equals(loginname) && possibleLogins.matches("(.*,)?loginname(,.*)?")) {
-        loginname = logincredential;
+      loginname = logincredential;
     }
     if (noRedirect != null) {
       getContext().put("ajax", noRedirect);
     }
-    return getContext().getWiki().getAuthService().checkAuth(loginname, password, 
-        rememberme, getContext());
+    return getContext().getWiki().getAuthService().checkAuth(loginname, password, rememberme,
+        getContext());
   }
 
   /**
    * API to check rights on a document for a given user or group
    * 
-   * @param level right to check (view, edit, comment, delete)
-   * @param user user or group for which to check the right
-   * @param isUser true for users and false for group
-   * @param docname document on which to check the rights
+   * @param level
+   *          right to check (view, edit, comment, delete)
+   * @param user
+   *          user or group for which to check the right
+   * @param isUser
+   *          true for users and false for group
+   * @param docname
+   *          document on which to check the rights
    * @return true if right is granted/false if not
    */
-  public boolean hasAccessLevel(String level, String user, boolean isUser,
-      DocumentReference docRef) throws XWikiException {
-    return ((XWikiRightServiceImpl) getContext().getWiki().getRightService()
-        ).hasAccessLevel(level, user, webUtilsService.getRefDefaultSerializer(
-            ).serialize(docRef), isUser, getContext());
+  public boolean hasAccessLevel(String level, String user, boolean isUser, DocumentReference docRef)
+      throws XWikiException {
+    return ((XWikiRightServiceImpl) getContext().getWiki().getRightService()).hasAccessLevel(level,
+        user, webUtilsService.getRefDefaultSerializer().serialize(docRef), isUser, getContext());
   }
-  
+
 }
--- src/main/java/com/celements/auth/IAuthenticationServiceRole.java
+++ src/main/java/com/celements/auth/IAuthenticationServiceRole.java
@@ -10,16 +10,16 @@ import com.xpn.xwiki.user.api.XWikiUser;
 
 @ComponentRole
 public interface IAuthenticationServiceRole {
-  
+
   public String getPasswordHash(String encoding, String str);
-  
-  public Map<String, String> activateAccount(String activationCode
-      ) throws AccountActivationFailedException;
 
-  public XWikiUser checkAuth(String logincredential, String password,
-      String rememberme, String possibleLogins, Boolean noRedirect) throws XWikiException;
+  public Map<String, String> activateAccount(String activationCode)
+      throws AccountActivationFailedException;
 
-  public boolean hasAccessLevel(String level, String user, boolean isUser,
-      DocumentReference docRef) throws XWikiException;
+  public XWikiUser checkAuth(String logincredential, String password, String rememberme,
+      String possibleLogins, Boolean noRedirect) throws XWikiException;
 
-  }
+  public boolean hasAccessLevel(String level, String user, boolean isUser, DocumentReference docRef)
+      throws XWikiException;
+
+}
