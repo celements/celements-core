a93937a6dff0225657fb722bbc4df3b5fc111c33
Marc Sladek <marc.sladek@synventis.com>
2016-08-23 15:52:46 +0200

    added logging to AbstractClassPackage.isActivated()

--- src/main/java/com/celements/model/classes/AbstractClassPackage.java
+++ src/main/java/com/celements/model/classes/AbstractClassPackage.java
@@ -2,21 +2,27 @@ package com.celements.model.classes;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Requirement;
 import org.xwiki.configuration.ConfigurationSource;
 
 public abstract class AbstractClassPackage implements ClassPackage {
+
+  protected Logger LOGGER = LoggerFactory.getLogger(ClassPackage.class);
 
   @Requirement
   protected ConfigurationSource configSrc;
 
   @Override
   public boolean isActivated() {
+    boolean ret = false;
     Object prop = configSrc.getProperty(CFG_SRC_KEY);
     if (prop instanceof List) {
-      return ((List<?>) prop).contains(getName());
+      ret = ((List<?>) prop).contains(getName());
     }
-    return false;
+    LOGGER.debug("{}: isActivated '{}' for property value '{}'", getName(), ret, prop);
+    return ret;
   }
 
 }
