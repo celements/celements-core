3a45fec6d6c6b70691f283dff6fca0418fce20c5
Marc Sladek <marc.sladek@synventis.com>
2016-08-25 11:52:13 +0200

    renamed exceptions

--- src/main/java/com/celements/model/access/exception/ModelAccessRuntimeException.java
+++ src/main/java/com/celements/model/access/exception/ModelAccessRuntimeException.java
@@ -2,18 +2,18 @@ package com.celements.model.access.exception;
 
 import org.xwiki.model.reference.EntityReference;
 
-public class DocumentAccessRuntimeException extends RuntimeException {
+public class ModelAccessRuntimeException extends RuntimeException {
 
   private static final long serialVersionUID = 2808580562362027580L;
 
   private final EntityReference ref;
 
-  public DocumentAccessRuntimeException(EntityReference ref) {
+  public ModelAccessRuntimeException(EntityReference ref) {
     super(ref != null ? ref.toString() : "null");
     this.ref = ref;
   }
 
-  public DocumentAccessRuntimeException(EntityReference ref, Throwable cause) {
+  public ModelAccessRuntimeException(EntityReference ref, Throwable cause) {
     super(ref != null ? ref.toString() : "null", cause);
     this.ref = ref;
   }
