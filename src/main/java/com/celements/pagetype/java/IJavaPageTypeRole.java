59dc3a0617bfa5d3e88b2d0f3aa5bf73fcb247f6
Fabian Pichler <fabian.pichler@synventis.com>
2016-09-22 21:12:46 +0200

    add NotNull annotations.

--- src/main/java/com/celements/pagetype/java/IJavaPageTypeRole.java
+++ src/main/java/com/celements/pagetype/java/IJavaPageTypeRole.java
@@ -2,6 +2,8 @@ package com.celements.pagetype.java;
 
 import java.util.Set;
 
+import javax.validation.constraints.NotNull;
+
 import org.xwiki.component.annotation.ComponentRole;
 import org.xwiki.model.reference.DocumentReference;
 
@@ -16,24 +18,30 @@ public interface IJavaPageTypeRole {
    * must be a unique name accross an installation. Two implementations with identical
    * names will randomly overwrite each other.
    */
+  @NotNull
   public String getName();
 
+  @NotNull
   public Set<IPageTypeCategoryRole> getCategories();
 
+  @NotNull
   public Set<String> getCategoryNames();
 
   public boolean hasPageTitle();
 
   public boolean displayInFrameLayout();
 
+  @NotNull
   public String getRenderTemplateForRenderMode(String renderMode);
 
   public boolean isVisible();
 
   public boolean isUnconnectedParent();
 
+  @NotNull
   public Optional<String> defaultTagName();
 
-  public void collectAttributes(AttributeBuilder attrBuilder, DocumentReference cellDocRef);
+  public void collectAttributes(@NotNull AttributeBuilder attrBuilder,
+      @NotNull DocumentReference cellDocRef);
 
 }
