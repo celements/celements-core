7e3480ef8135148731f95067a3a98a3c4fa19b34
Dari Khasham <dari.khasham@synventis.com>
2016-09-27 18:19:58 +0200

    added LargeStringField Class

---
+++ src/main/java/com/celements/model/classes/fields/LargeStringField.java
@@ -0,0 +1,62 @@
+package com.celements.model.classes.fields;
+
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.Immutable;
+import javax.validation.constraints.NotNull;
+
+import com.xpn.xwiki.objects.classes.StringClass;
+import com.xpn.xwiki.objects.classes.TextAreaClass;
+
+@Immutable
+public final class LargeStringField extends StringField {
+
+  private final Integer rows;
+
+  public static class Builder extends StringField.Builder {
+
+    private Integer rows;
+
+    public Builder(@NotNull String classDefName, @NotNull String name) {
+      super(classDefName, name);
+      this.rows(rows);
+    }
+
+    @Override
+    public Builder getThis() {
+      return this;
+    }
+
+    public Builder rows(@Nullable Integer val) {
+      rows = val;
+      return getThis();
+    }
+
+    @Override
+    public LargeStringField build() {
+      return new LargeStringField(getThis());
+    }
+
+  }
+
+  protected LargeStringField(@NotNull Builder builder) {
+    super(builder);
+    this.rows = builder.rows;
+  }
+
+  @Override
+  public Class<String> getType() {
+    return String.class;
+  }
+
+  public Integer getRow() {
+    return rows;
+  }
+
+  @Override
+  public StringClass getStringPropertyClass() {
+    TextAreaClass textAreaClass = new TextAreaClass();
+    textAreaClass.setRows(rows);
+    return textAreaClass;
+  }
+
+}
