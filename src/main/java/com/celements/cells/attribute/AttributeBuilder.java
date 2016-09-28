4273701658066a42d86bc8e1a8261a1f9b0b2aaa
Fabian Pichler <fabian.pichler@synventis.com>
2016-09-28 20:17:18 +0200

    add addAttribute to AttributeBuilder

--- src/main/java/com/celements/cells/attribute/AttributeBuilder.java
+++ src/main/java/com/celements/cells/attribute/AttributeBuilder.java
@@ -7,10 +7,29 @@ import javax.validation.constraints.NotNull;
 
 public interface AttributeBuilder {
 
+  /**
+   * addAttribute adds <code>attrName</code> with <code>attrValue</code>.
+   * If <code>attrValue</code> is null or empty it adds an empty value.
+   */
+  @NotNull
+  AttributeBuilder addAttribute(@NotNull String attrName, @Nullable String attrValue);
+
+  /**
+   * addEmptyAttribute adds <code>attrName</code> with an absent value.
+   * For XHTML this should be rendered as e.g. <code>selected="selected"</code> and for HTML5 as
+   * e.g. <code>selected</code>
+   */
+  @NotNull
   AttributeBuilder addEmptyAttribute(@NotNull String attrName);
 
+  /**
+   * addNonEmptyAttribute only adds <code>attrName</code> if <code>attrValue</code> is not null and
+   * not empty. If <code>attrValue</code> is null or empty it just returns.
+   */
+  @NotNull
   AttributeBuilder addNonEmptyAttribute(@NotNull String attrName, @Nullable String attrValue);
 
+  @NotNull
   AttributeBuilder addNonEmptyAttribute(@NotNull String attrName,
       @NotNull Iterable<String> attrValues);
 
