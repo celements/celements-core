f288e3a12c8c48d55fef8dfdea841fde070aab37
ebeutler <edoardo.beutler@synventis.com>
2016-12-03 12:58:27 +0100

    add javadoc

--- src/main/java/com/celements/web/classcollections/oldcore/IOldCoreClassDef.java
+++ src/main/java/com/celements/web/classcollections/oldcore/IOldCoreClassDef.java
@@ -4,6 +4,14 @@ import org.xwiki.component.annotation.ComponentRole;
 
 import com.celements.model.classes.ClassDefinition;
 
+/**
+ * Do NOT add this to a collection. Components for this role are solely for the purpose of having a
+ * class and constants where used in Java. Loading this class would interfere with classes handled
+ * by XWiki.
+ *
+ * @author ebeutler
+ * @version $Revision: 1.1 $
+ */
 @ComponentRole
 public interface IOldCoreClassDef extends ClassDefinition {
 
