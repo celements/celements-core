0090064a70e994122b5bff4a4c645b2b36d5d25e
ebeutler <edoardo.beutler@synventis.com>
2017-01-08 21:47:22 +0100

    add enumeration tests

---
+++ src/test/java/com/celements/metatag/enums/ECharsetTest.java
@@ -0,0 +1,37 @@
+package com.celements.metatag.enums;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.celements.metatag.enums.opengraph.EOpenGraph;
+
+public class ECharsetTest {
+
+  private static String FIELDS_UTF8 = "UTF-8";
+  private static String FIELDS_LATIN1 = "ISO-8859-1";
+  private static String FIELDS_USASCII = "US-ASCII";
+
+  @Test
+  public void testAttribs() {
+    assertEquals("property", EOpenGraph.ATTRIB_NAME);
+  }
+
+  @Test
+  public void testGetCharset() {
+    assertTrue(ECharset.getCharset(FIELDS_UTF8).equals(ECharset.UTF8) || ECharset.getCharset(
+        FIELDS_UTF8).equals(ECharset.DEFAULT));
+    assertTrue(ECharset.getCharset(FIELDS_LATIN1).equals(ECharset.LATIN1) || ECharset.getCharset(
+        FIELDS_LATIN1).equals(ECharset.ISO8859_1));
+    assertEquals(ECharset.USASCII, ECharset.getCharset(FIELDS_USASCII));
+  }
+
+  @Test
+  public void testGetIdentifier() {
+    assertEquals(FIELDS_UTF8, ECharset.UTF8.getIdentifier());
+    assertEquals(FIELDS_LATIN1, ECharset.LATIN1.getIdentifier());
+    assertEquals(FIELDS_LATIN1, ECharset.ISO8859_1.getIdentifier());
+    assertEquals(FIELDS_USASCII, ECharset.USASCII.getIdentifier());
+    assertEquals(FIELDS_UTF8, ECharset.DEFAULT.getIdentifier());
+  }
+}
---
+++ src/test/java/com/celements/metatag/enums/ENameNonStandardTest.java
@@ -0,0 +1,41 @@
+package com.celements.metatag.enums;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ENameNonStandardTest {
+
+  private static String FIELDS_CREATOR = "creator";
+  private static String FIELDS_GOOGLEBOT = "googlebot";
+  private static String FIELDS_PUBLISHER = "publisher";
+  private static String FIELDS_ROBOTS = "robots";
+  private static String FIELDS_SLURP = "slurp";
+  private static String FIELDS_VIEWPORT = "viewport";
+
+  @Test
+  public void testAttribs() {
+    assertEquals("name", ENameStandard.ATTRIB_NAME);
+    assertEquals("property", ENameStandard.ATTRIB_NAME_ALT);
+  }
+
+  @Test
+  public void testGetName() {
+    assertEquals(ENameNonStandard.CREATOR, ENameNonStandard.getName(FIELDS_CREATOR));
+    assertEquals(ENameNonStandard.GOOGLEBOT, ENameNonStandard.getName(FIELDS_GOOGLEBOT));
+    assertEquals(ENameNonStandard.PUBLISHER, ENameNonStandard.getName(FIELDS_PUBLISHER));
+    assertEquals(ENameNonStandard.ROBOTS, ENameNonStandard.getName(FIELDS_ROBOTS));
+    assertEquals(ENameNonStandard.SLURP, ENameNonStandard.getName(FIELDS_SLURP));
+    assertEquals(ENameNonStandard.VIEWPORT, ENameNonStandard.getName(FIELDS_VIEWPORT));
+  }
+
+  @Test
+  public void testGetIdentifier() {
+    assertEquals(FIELDS_CREATOR, ENameNonStandard.CREATOR.getIdentifier());
+    assertEquals(FIELDS_GOOGLEBOT, ENameNonStandard.GOOGLEBOT.getIdentifier());
+    assertEquals(FIELDS_PUBLISHER, ENameNonStandard.PUBLISHER.getIdentifier());
+    assertEquals(FIELDS_ROBOTS, ENameNonStandard.ROBOTS.getIdentifier());
+    assertEquals(FIELDS_SLURP, ENameNonStandard.SLURP.getIdentifier());
+    assertEquals(FIELDS_VIEWPORT, ENameNonStandard.VIEWPORT.getIdentifier());
+  }
+}
