ad9515788b4032c64ef0d243fb61c99445feae9c
Gal-Cohen08 <galcohen08@gmail.com>
2017-01-09 16:55:30 +0100

    implemented two ClassDefinitions

---
+++ src/main/java/classes/PageTypeClassDefinition.java
@@ -0,0 +1,18 @@
+package classes;
+
+import org.xwiki.component.annotation.Component;
+
+import com.celements.model.classes.fields.ClassField;
+import com.celements.model.classes.fields.StringField;
+
+@Component(PageTypeClassDefinition.CLASS_DEF_HINT)
+public class PageTypeClassDefinition {
+
+  public static final String SPACE_NAME = "Celements2";
+  public static final String DOC_NAME = "PageType";
+  public static final String CLASS_DEF_HINT = SPACE_NAME + "." + DOC_NAME;
+
+  public static final ClassField<String> PAGE_TYPE_FIELD = new StringField.Builder(CLASS_DEF_HINT,
+      "page_type").size(30).prettyName("Page Type").build();
+
+}
