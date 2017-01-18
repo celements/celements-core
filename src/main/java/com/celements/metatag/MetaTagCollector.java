576f8861f69e5a1b5ed6fc741dbb6688e0320426
ebeutler <edoardo.beutler@synventis.com>
2017-01-18 09:44:37 +0100

    extract method getMetaTags

--- src/main/java/com/celements/metatag/MetaTagCollector.java
+++ src/main/java/com/celements/metatag/MetaTagCollector.java
@@ -17,29 +17,34 @@ public class MetaTagCollector implements MetaTagCollectorRole {
   @Override
   public void addMetaTag(MetaTagApi tag) {
     Object contextObj = modelContext.getXWikiContext().get(MetaTagCollectorRole.META_CONTEXT_KEY);
-    if (contextObj instanceof List) {
-      @SuppressWarnings("unchecked")
-      List<MetaTagApi> metaList = (List<MetaTagApi>) contextObj;
-      metaList.add(tag);
-    } else {
-      List<MetaTagApi> metaList = new ArrayList<>();
-      metaList.add(tag);
+    List<MetaTagApi> metaList = getMetaTags(contextObj);
+    if (metaList == null) {
+      metaList = new ArrayList<>();
       modelContext.getXWikiContext().put(MetaTagCollectorRole.META_CONTEXT_KEY, metaList);
     }
+    metaList.add(tag);
   }
 
   @Override
   public String displayAllMetaTags() {
     StringBuilder sb = new StringBuilder();
-    Object contextObj = modelContext.getXWikiContext().remove(MetaTagCollectorRole.META_CONTEXT_KEY);
-    if (contextObj instanceof List) {
-      @SuppressWarnings("unchecked")
-      List<MetaTagApi> metaList = (List<MetaTagApi>) contextObj;
-      for (MetaTagApi metaTag : metaList) {
+    Object contextObj = modelContext.getXWikiContext().remove(
+        MetaTagCollectorRole.META_CONTEXT_KEY);
+    List<MetaTagApi> metaTags = getMetaTags(contextObj);
+    if (metaTags != null) {
+      for (MetaTagApi metaTag : metaTags) {
         sb.append(metaTag.display()).append("\n");
       }
     }
     return sb.toString();
+  }
+
+  @SuppressWarnings("unchecked")
+  List<MetaTagApi> getMetaTags(Object contextObj) {
+    if (contextObj instanceof List) {
+      return (List<MetaTagApi>) contextObj;
+    }
+    return null;
   }
 
 }
