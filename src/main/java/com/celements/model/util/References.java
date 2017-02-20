0e71fe68cefc21a8094218393162229fc91002ba
Marc Sladek <marc.sladek@synventis.com>
2017-02-20 16:57:18 +0100

    extracted EntityTypeUtil

--- src/main/java/com/celements/model/util/References.java
+++ src/main/java/com/celements/model/util/References.java
@@ -1,108 +1,17 @@
 package com.celements.model.util;
 
+import static com.celements.model.util.EntityTypeUtil.*;
 import static com.google.common.base.Preconditions.*;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
 
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 
 import org.xwiki.model.EntityType;
-import org.xwiki.model.reference.AttachmentReference;
-import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.EntityReference;
-import org.xwiki.model.reference.ObjectPropertyReference;
-import org.xwiki.model.reference.ObjectReference;
-import org.xwiki.model.reference.SpaceReference;
-import org.xwiki.model.reference.WikiReference;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.BiMap;
-import com.google.common.collect.ImmutableBiMap;
 
 public class References {
-
-  private static final BiMap<Class<? extends EntityReference>, EntityType> ENTITY_TYPE_MAP;
-  private static final Map<Class<? extends EntityReference>, String> REGEX_MAP;
-
-  public static final String REGEX_WORD = "[a-zA-Z0-9_-]+";
-  public static final String REGEX_WIKINAME = "[a-zA-Z0-9]+";
-  public static final String REGEX_SPACE = "(" + REGEX_WIKINAME + "\\:)?" + REGEX_WORD;
-  public static final String REGEX_DOC = REGEX_SPACE + "\\." + REGEX_WORD;
-  public static final String REGEX_ATT = REGEX_DOC + "\\@" + ".+";
-
-  static {
-    Map<Class<? extends EntityReference>, EntityType> map = new HashMap<>();
-    map.put(WikiReference.class, EntityType.WIKI);
-    map.put(SpaceReference.class, EntityType.SPACE);
-    map.put(DocumentReference.class, EntityType.DOCUMENT);
-    map.put(AttachmentReference.class, EntityType.ATTACHMENT);
-    map.put(ObjectReference.class, EntityType.OBJECT);
-    map.put(ObjectPropertyReference.class, EntityType.OBJECT_PROPERTY);
-    ENTITY_TYPE_MAP = ImmutableBiMap.copyOf(map);
-    Map<Class<? extends EntityReference>, String> regexMap = new LinkedHashMap<>();
-    regexMap.put(WikiReference.class, REGEX_WIKINAME);
-    regexMap.put(SpaceReference.class, REGEX_SPACE);
-    regexMap.put(DocumentReference.class, REGEX_DOC);
-    regexMap.put(AttachmentReference.class, REGEX_ATT);
-    REGEX_MAP = Collections.unmodifiableMap(regexMap);
-  }
-
-  public static EntityType getEntityTypeForClass(Class<? extends EntityReference> token) {
-    EntityType type = ENTITY_TYPE_MAP.get(checkNotNull(token));
-    if (type != null) {
-      return type;
-    } else {
-      throw new IllegalArgumentException("No entity type for class: " + token);
-    }
-  }
-
-  public static Class<? extends EntityReference> getClassForEntityType(EntityType type) {
-    Class<? extends EntityReference> token = ENTITY_TYPE_MAP.inverse().get(checkNotNull(type));
-    if (token != null) {
-      return token;
-    } else {
-      throw new IllegalArgumentException("No class for entity type: " + type);
-    }
-  }
-
-  /**
-   * @return the class for the root entity type
-   */
-  public static Class<? extends EntityReference> getRootClass() {
-    return ENTITY_TYPE_MAP.inverse().get(EntityType.values()[0]);
-  }
-
-  /**
-   * identifies the reference class for the given absolute name (root type may be missing).<br>
-   * <br>
-   * simple names default to the root entity type.
-   *
-   * @param name
-   *          the string representation
-   * @return the identified reference class
-   * @throws IllegalArgumentException
-   *           for illegal strings
-   */
-  @NotNull
-  public static Class<? extends EntityReference> identifyClassFromName(@NotNull String name) {
-    if (!checkNotNull(name).isEmpty()) {
-      Set<Class<? extends EntityReference>> tokens = new LinkedHashSet<>(); // keeps insertion order
-      tokens.add(getRootClass());
-      tokens.addAll(REGEX_MAP.keySet());
-      for (Class<? extends EntityReference> token : tokens) {
-        if (name.matches(REGEX_MAP.get(token))) {
-          return token;
-        }
-      }
-    }
-    throw new IllegalArgumentException("No valid reference class found for '" + name + "'");
-  }
 
   /**
    * @param ref
@@ -127,10 +36,8 @@ public class References {
    */
   @NotNull
   public static EntityReference cloneRef(@NotNull EntityReference ref) {
-    Class<? extends EntityReference> token = EntityReference.class;
-    if (isAbsoluteRef(ref)) {
-      token = ENTITY_TYPE_MAP.inverse().get(ref.getType());
-    }
+    Class<? extends EntityReference> token = isAbsoluteRef(ref) ? getClassForEntityType(
+        ref.getType()) : EntityReference.class;
     return cloneRef(ref, token);
   }
 
@@ -175,8 +82,9 @@ public class References {
   public static <T extends EntityReference> Optional<T> extractRef(
       @Nullable EntityReference fromRef, @NotNull Class<T> token) {
     EntityReference extractedRef = null;
-    if (fromRef != null) {
-      extractedRef = fromRef.extractReference(getEntityTypeForClass(token));
+    Optional<EntityType> type = getEntityTypeForClass(token);
+    if ((fromRef != null) && type.isPresent()) {
+      extractedRef = fromRef.extractReference(type.get());
     }
     if (extractedRef != null) {
       return Optional.of(cloneRef(extractedRef, token));
@@ -232,7 +140,12 @@ public class References {
 
   public static <T extends EntityReference> T create(@NotNull Class<T> token, @NotNull String name,
       @Nullable EntityReference parent) {
-    return createInternal(token, getEntityTypeForClass(token), name, parent);
+    Optional<EntityType> type = getEntityTypeForClass(token);
+    if (type.isPresent()) {
+      return createInternal(token, type.get(), name, parent);
+    } else {
+      throw new IllegalArgumentException("No entity type for class: " + token);
+    }
   }
 
   private static <T extends EntityReference> T createInternal(@NotNull Class<T> token,
--- src/test/java/com/celements/model/util/ReferencesTest.java
+++ src/test/java/com/celements/model/util/ReferencesTest.java
@@ -1,5 +1,6 @@
 package com.celements.model.util;
 
+import static com.celements.model.util.References.*;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -34,31 +35,25 @@ public class ReferencesTest extends AbstractComponentTest {
   }
 
   @Test
-  public void test_rootRefClass() {
-    assertSame("WikiReference has to be the root reference", WikiReference.class,
-        References.getRootClass());
-  }
-
-  @Test
   public void test_isAbsoluteRef() {
-    assertTrue(References.isAbsoluteRef(wikiRef));
-    assertTrue(References.isAbsoluteRef(spaceRef));
-    assertTrue(References.isAbsoluteRef(docRef));
-    assertTrue(References.isAbsoluteRef(attRef));
+    assertTrue(isAbsoluteRef(wikiRef));
+    assertTrue(isAbsoluteRef(spaceRef));
+    assertTrue(isAbsoluteRef(docRef));
+    assertTrue(isAbsoluteRef(attRef));
     ObjectReference objRef = new ObjectReference("Class.Obj", docRef);
-    assertTrue(References.isAbsoluteRef(objRef));
+    assertTrue(isAbsoluteRef(objRef));
     // ObjectPropertyReference is buggy, always contains EntityType.OBJECT, see setType
-    // assertTrue(References.isAbsoluteRef(new ObjectPropertyReference("field", objRef)));
-    assertFalse(References.isAbsoluteRef(getRelativeRefResolver().resolve(
-        modelUtils.serializeRefLocal(docRef), EntityType.DOCUMENT)));
-    assertTrue(References.isAbsoluteRef(getRelativeRefResolver().resolve(modelUtils.serializeRef(
-        docRef), EntityType.DOCUMENT)));
+    // assertTrue(isAbsoluteRef(new ObjectPropertyReference("field", objRef)));
+    assertFalse(isAbsoluteRef(getRelativeRefResolver().resolve(modelUtils.serializeRefLocal(docRef),
+        EntityType.DOCUMENT)));
+    assertTrue(isAbsoluteRef(getRelativeRefResolver().resolve(modelUtils.serializeRef(docRef),
+        EntityType.DOCUMENT)));
   }
 
   @Test
   public void test_cloneRef() {
     DocumentReference ref = docRef;
-    EntityReference newRef = References.cloneRef(ref);
+    EntityReference newRef = cloneRef(ref);
     assertTrue(newRef instanceof DocumentReference);
     assertNotSame(ref, newRef);
     assertEquals(ref, newRef);
@@ -69,7 +64,7 @@ public class ReferencesTest extends AbstractComponentTest {
   @Test
   public void test_cloneRef_wikiRef() {
     WikiReference ref = wikiRef;
-    WikiReference newRef = References.cloneRef(ref, WikiReference.class);
+    WikiReference newRef = cloneRef(ref, WikiReference.class);
     assertNotSame(ref, newRef);
     assertEquals(ref, newRef);
     ref.setName("asdf");
@@ -79,7 +74,7 @@ public class ReferencesTest extends AbstractComponentTest {
   @Test
   public void test_cloneRef_spaceRef() {
     SpaceReference ref = spaceRef;
-    SpaceReference newRef = References.cloneRef(ref, SpaceReference.class);
+    SpaceReference newRef = cloneRef(ref, SpaceReference.class);
     assertNotSame(ref, newRef);
     assertEquals(ref, newRef);
     ref.getParent().setName("asdf");
@@ -89,7 +84,7 @@ public class ReferencesTest extends AbstractComponentTest {
   @Test
   public void test_cloneRef_docRef() {
     DocumentReference ref = docRef;
-    DocumentReference newRef = References.cloneRef(ref, DocumentReference.class);
+    DocumentReference newRef = cloneRef(ref, DocumentReference.class);
     assertNotSame(ref, newRef);
     assertEquals(ref, newRef);
     ref.getParent().setName("asdf");
@@ -99,7 +94,7 @@ public class ReferencesTest extends AbstractComponentTest {
   @Test
   public void test_cloneRef_entityRef() {
     DocumentReference ref = docRef;
-    EntityReference newRef = References.cloneRef(ref, EntityReference.class);
+    EntityReference newRef = cloneRef(ref, EntityReference.class);
     assertTrue(newRef instanceof DocumentReference);
     assertNotSame(ref, newRef);
     assertEquals(ref, newRef);
@@ -112,7 +107,7 @@ public class ReferencesTest extends AbstractComponentTest {
     EntityReference ref = getRelativeRefResolver().resolve(modelUtils.serializeRefLocal(docRef),
         EntityType.DOCUMENT);
     try {
-      References.cloneRef(ref, DocumentReference.class);
+      cloneRef(ref, DocumentReference.class);
       fail("expecting failure, cannot clone relative reference to specific implementation");
     } catch (IllegalArgumentException iae) {
     }
@@ -122,7 +117,7 @@ public class ReferencesTest extends AbstractComponentTest {
   public void test_cloneRef_relative_asEntityRef() {
     EntityReference ref = getRelativeRefResolver().resolve(modelUtils.serializeRefLocal(docRef),
         EntityType.DOCUMENT);
-    EntityReference newRef = References.cloneRef(ref);
+    EntityReference newRef = cloneRef(ref);
     assertFalse(newRef instanceof DocumentReference);
     assertNotSame(ref, newRef);
     assertEquals(ref, newRef);
@@ -131,49 +126,19 @@ public class ReferencesTest extends AbstractComponentTest {
   }
 
   @Test
-  public void test_identifyClassFromName() {
-    assertEquals(WikiReference.class, References.identifyClassFromName("wiki"));
-    assertEquals(SpaceReference.class, References.identifyClassFromName("wiki:space"));
-    assertEquals(DocumentReference.class, References.identifyClassFromName("wiki:space.doc"));
-    assertEquals(DocumentReference.class, References.identifyClassFromName("space.doc"));
-    assertEquals(AttachmentReference.class, References.identifyClassFromName(
-        "wiki:space.doc@att.jpg"));
-    assertEquals(AttachmentReference.class, References.identifyClassFromName("space.doc@att.jpg"));
-    try {
-      References.identifyClassFromName("doc@att");
-      fail("expecting failure because of relative ref");
-    } catch (IllegalArgumentException iae) {
-    }
-  }
-
-  @Test
-  public void test_identifyClassFromName_specialChars() {
-    assertEquals(SpaceReference.class, References.identifyClassFromName("wiki:space_test"));
-    assertEquals(SpaceReference.class, References.identifyClassFromName("wiki:space-test"));
-    assertEquals(DocumentReference.class, References.identifyClassFromName("wiki:space.doc_test"));
-    assertEquals(DocumentReference.class, References.identifyClassFromName("wiki:space.doc-test"));
-    assertEquals(DocumentReference.class, References.identifyClassFromName("space.doc_test"));
-    assertEquals(DocumentReference.class, References.identifyClassFromName("space.doc-test"));
-    assertEquals(AttachmentReference.class, References.identifyClassFromName(
-        "wiki:space.doc@att-test.jpg"));
-    assertEquals(AttachmentReference.class, References.identifyClassFromName(
-        "wiki:space.doc@att_test.jpg"));
-  }
-
-  @Test
   public void test_extractRef() {
-    assertEquals(wikiRef, References.extractRef(docRef, WikiReference.class).get());
-    assertEquals(spaceRef, References.extractRef(docRef, SpaceReference.class).get());
-    assertEquals(docRef, References.extractRef(docRef, DocumentReference.class).get());
-    assertFalse(References.extractRef(docRef, AttachmentReference.class).isPresent());
-    assertEquals(attRef, References.extractRef(attRef, AttachmentReference.class).get());
-    assertNotSame(docRef, References.extractRef(docRef, DocumentReference.class).get());
+    assertEquals(wikiRef, extractRef(docRef, WikiReference.class).get());
+    assertEquals(spaceRef, extractRef(docRef, SpaceReference.class).get());
+    assertEquals(docRef, extractRef(docRef, DocumentReference.class).get());
+    assertFalse(extractRef(docRef, AttachmentReference.class).isPresent());
+    assertEquals(attRef, extractRef(attRef, AttachmentReference.class).get());
+    assertNotSame(docRef, extractRef(docRef, DocumentReference.class).get());
   }
 
   @Test
   public void test_adjustRef_higherLevel_1() {
     EntityReference toRef = new WikiReference("oWiki");
-    DocumentReference ret = References.adjustRef(docRef, DocumentReference.class, toRef);
+    DocumentReference ret = adjustRef(docRef, DocumentReference.class, toRef);
     assertEquals(toRef, ret.getWikiReference());
     assertEquals(docRef.getName(), ret.getName());
     assertEquals(spaceRef.getName(), ret.getLastSpaceReference().getName());
@@ -183,7 +148,7 @@ public class ReferencesTest extends AbstractComponentTest {
   @Test
   public void test_adjustRef_higherLevel_2() {
     EntityReference toRef = new SpaceReference("oSpace", new WikiReference("oWiki"));
-    DocumentReference ret = References.adjustRef(docRef, DocumentReference.class, toRef);
+    DocumentReference ret = adjustRef(docRef, DocumentReference.class, toRef);
     assertEquals(toRef, ret.getLastSpaceReference());
     assertEquals(docRef.getName(), ret.getName());
     assertEquals("space should have changed", toRef, ret.getLastSpaceReference());
@@ -192,7 +157,7 @@ public class ReferencesTest extends AbstractComponentTest {
   @Test
   public void test_adjustRef_sameLevel() {
     EntityReference toRef = new DocumentReference("oWiki", "oSpace", "oDoc");
-    DocumentReference ret = References.adjustRef(docRef, DocumentReference.class, toRef);
+    DocumentReference ret = adjustRef(docRef, DocumentReference.class, toRef);
     assertEquals("expecting toRef if same level entity", toRef, ret);
     assertNotSame(toRef, ret);
     assertEquals("doc should have changed", toRef, ret);
@@ -202,7 +167,7 @@ public class ReferencesTest extends AbstractComponentTest {
   public void test_adjustRef_lowerLevel() {
     EntityReference toRef = new AttachmentReference("oAtt", new DocumentReference("oWiki", "oSpace",
         "oDoc"));
-    DocumentReference ret = References.adjustRef(docRef, DocumentReference.class, toRef);
+    DocumentReference ret = adjustRef(docRef, DocumentReference.class, toRef);
     assertEquals("expecting docRef if lower level entity", docRef, ret);
     assertNotSame(docRef, ret);
   }
@@ -211,7 +176,7 @@ public class ReferencesTest extends AbstractComponentTest {
   public void test_adjustRef_childUnchanged() {
     WikiReference toRef = new WikiReference("oWiki");
     assertNull(toRef.getChild());
-    References.adjustRef(new EntityReference("doc", EntityType.DOCUMENT, new EntityReference("wiki",
+    adjustRef(new EntityReference("doc", EntityType.DOCUMENT, new EntityReference("wiki",
         EntityType.WIKI)), EntityReference.class, toRef);
     // EntityReference.setParent overwrites child of param, ensure it is cloned
     assertNull(toRef.getChild());
@@ -220,7 +185,7 @@ public class ReferencesTest extends AbstractComponentTest {
   @Test
   public void test_create_wiki() {
     String name = "wiki";
-    WikiReference wikiRef = References.create(WikiReference.class, name);
+    WikiReference wikiRef = create(WikiReference.class, name);
     assertNotNull(wikiRef);
     assertEquals(name, wikiRef.getName());
   }
@@ -228,7 +193,7 @@ public class ReferencesTest extends AbstractComponentTest {
   @Test
   public void test_create_space() {
     String name = "space";
-    SpaceReference spaceRef = References.create(SpaceReference.class, name, wikiRef);
+    SpaceReference spaceRef = create(SpaceReference.class, name, wikiRef);
     assertNotNull(spaceRef);
     assertEquals(name, spaceRef.getName());
     assertEquals(wikiRef, spaceRef.getParent());
@@ -237,7 +202,7 @@ public class ReferencesTest extends AbstractComponentTest {
   @Test
   public void test_create_doc() {
     String name = "doc";
-    DocumentReference docRef = References.create(DocumentReference.class, name, spaceRef);
+    DocumentReference docRef = create(DocumentReference.class, name, spaceRef);
     assertNotNull(docRef);
     assertEquals(name, docRef.getName());
     assertEquals(spaceRef, docRef.getParent());
@@ -246,7 +211,7 @@ public class ReferencesTest extends AbstractComponentTest {
   @Test
   public void test_create_entity() {
     String name = "doc";
-    EntityReference docRef = References.create(EntityType.DOCUMENT, name, spaceRef);
+    EntityReference docRef = create(EntityType.DOCUMENT, name, spaceRef);
     assertNotNull(docRef);
     assertEquals(name, docRef.getName());
     assertEquals(spaceRef, docRef.getParent());
@@ -255,7 +220,7 @@ public class ReferencesTest extends AbstractComponentTest {
   @Test
   public void test_create_childparent() {
     WikiReference wikiRef = new WikiReference("wiki");
-    SpaceReference spaceRef = References.create(SpaceReference.class, "space", wikiRef);
+    SpaceReference spaceRef = create(SpaceReference.class, "space", wikiRef);
     assertNotSame(wikiRef, spaceRef.getParent());
     assertEquals(wikiRef, spaceRef.getParent());
     assertSame(spaceRef, spaceRef.getParent().getChild());
