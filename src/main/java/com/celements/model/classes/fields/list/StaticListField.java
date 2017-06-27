a49be809c8e5e22a2cac9fe6750c09ee71cb5209
Marc Sladek <marc.sladek@synventis.com>
2017-06-27 20:45:10 +0200

    fix StaticListField inheritance

--- src/main/java/com/celements/model/classes/fields/list/StaticListField.java
+++ src/main/java/com/celements/model/classes/fields/list/StaticListField.java
@@ -6,6 +6,19 @@ import javax.validation.constraints.NotNull;
 @Immutable
 public final class StaticListField extends StringListField {
 
+  public static class Builder extends StringListField.Builder<Builder> {
+
+    public Builder(@NotNull String classDefName, @NotNull String name) {
+      super(classDefName, name);
+    }
+
+    @Override
+    public StaticListField build() {
+      return new StaticListField(this);
+    }
+
+  }
+
   protected StaticListField(@NotNull Builder builder) {
     super(builder);
   }
