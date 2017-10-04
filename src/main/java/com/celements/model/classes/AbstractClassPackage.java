09748764ae1e17a6a87cde89d4c81d81a470f9cc
Marc Sladek <marc.sladek@synventis.com>
2017-10-04 18:47:07 +0200

    multiple isActivated checks in AbstractClassPackage

--- src/main/java/com/celements/model/classes/AbstractClassPackage.java
+++ src/main/java/com/celements/model/classes/AbstractClassPackage.java
@@ -7,11 +7,21 @@ import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Requirement;
 import org.xwiki.configuration.ConfigurationSource;
 
+import com.celements.configuration.CelementsPropertiesConfigurationSource;
 import com.celements.model.context.ModelContext;
 
 public abstract class AbstractClassPackage implements ClassPackage {
 
   protected Logger LOGGER = LoggerFactory.getLogger(ClassPackage.class);
+
+  @Requirement("xwikiproperties")
+  private ConfigurationSource xwikiPropertiesSource;
+
+  @Requirement(CelementsPropertiesConfigurationSource.NAME)
+  private ConfigurationSource celementsPropertiesSource;
+
+  @Requirement("wiki")
+  private ConfigurationSource wikiPreferencesSource;
 
   @Requirement
   protected ConfigurationSource configSrc;
@@ -21,6 +31,11 @@ public abstract class AbstractClassPackage implements ClassPackage {
 
   @Override
   public boolean isActivated() {
+    return isActivated(xwikiPropertiesSource) || isActivated(celementsPropertiesSource)
+        || isActivated(wikiPreferencesSource) || isActivated(configSrc);
+  }
+
+  private boolean isActivated(ConfigurationSource configSrc) {
     boolean activated = false;
     Object prop = configSrc.getProperty(CFG_SRC_KEY);
     if (prop instanceof Collection) {
@@ -28,7 +43,8 @@ public abstract class AbstractClassPackage implements ClassPackage {
     } else if (prop instanceof String) {
       activated = prop.toString().trim().equals(getName());
     }
-    LOGGER.debug("{}: isActivated '{}' for property value '{}'", getName(), activated, prop);
+    LOGGER.debug("{}: isActivated '{}' for config source '{}'", getName(), activated,
+        configSrc.getClass().getSimpleName());
     return activated;
   }
 
