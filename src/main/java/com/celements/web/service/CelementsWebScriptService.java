4c724d9b301c9aef37c3819e24cdfd87deb17f96
Dari Khasham <dari.khasham@synventis.com>
2017-10-17 15:28:12 +0200

    removed unused code

--- src/main/java/com/celements/web/service/CelementsWebScriptService.java
+++ src/main/java/com/celements/web/service/CelementsWebScriptService.java
@@ -1111,7 +1111,6 @@ public class CelementsWebScriptService implements ScriptService {
       LOGGER.error("Failed to encode url [" + urlStr + "] to utf-8", exp);
     }
     urlStr = "http://" + urlStr;
-    getContext().getResponse();
     return urlStr;
   }
 
