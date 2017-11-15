17fce64aee7e82112b6719df9aac37e6839843b7
Marc Sladek <marc.sladek@synventis.com>
2017-11-15 16:32:12 +0100

    NoStackTracePatternLayout hasComponent check (#142)

--- src/main/java/com/celements/log4j/NoStackTracePatternLayout.java
+++ src/main/java/com/celements/log4j/NoStackTracePatternLayout.java
@@ -43,7 +43,10 @@ public class NoStackTracePatternLayout extends PatternLayout {
   }
 
   private Level getNoStackTraceLevel() {
-    String levelName = Utils.getComponent(ConfigurationSource.class).getProperty(CFG_SRC_KEY_LEVEL);
+    String levelName = null;
+    if (Utils.getComponentManager().hasComponent(ConfigurationSource.class)) {
+      levelName = Utils.getComponent(ConfigurationSource.class).getProperty(CFG_SRC_KEY_LEVEL);
+    }
     return Level.toLevel(levelName, DEFAULT_LEVEL);
   }
 
