d8c3ebb7f429236e620587dbc0a0baa9d89b2a4a
Marc Sladek <marc.sladek@synventis.com>
2017-11-22 17:51:23 +0100

    ModelContext additional methods (#144)
    * add getRequestParameter and get/setURL
    
    * add unit tests
    
    * getXWikiContext stub creation

--- src/test/java/com/celements/model/context/DefaultModelContextTest.java
+++ src/test/java/com/celements/model/context/DefaultModelContextTest.java
@@ -4,6 +4,8 @@ import static com.celements.common.test.CelementsTestUtils.*;
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.easymock.IAnswer;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,8 +19,11 @@ import com.celements.common.test.AbstractComponentTest;
 import com.celements.configuration.CelementsFromWikiConfigurationSource;
 import com.celements.model.access.IModelAccessFacade;
 import com.celements.model.util.ModelUtils;
+import com.google.common.base.Optional;
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.web.Utils;
+import com.xpn.xwiki.web.XWikiServletRequest;
+import com.xpn.xwiki.web.XWikiServletRequestStub;
 
 public class DefaultModelContextTest extends AbstractComponentTest {
 
@@ -153,6 +158,38 @@ public class DefaultModelContextTest extends AbstractComponentTest {
     assertEquals(lang, modelContext.getDefaultLanguage(docRef));
     verifyDefault();
     assertDefaultContext();
+  }
+
+  @Test
+  public void test_getRequest_noRequest() {
+    assertFalse(modelContext.getRequest().isPresent());
+    assertFalse(modelContext.getRequestParameter("asdf").isPresent());
+  }
+
+  @Test
+  public void test_getRequest_withRequest() {
+    getContext().setRequest(new XWikiServletRequestStub());
+    assertTrue(modelContext.getRequest().isPresent());
+    assertSame(getContext().getRequest(), modelContext.getRequest().get());
+    assertFalse(modelContext.getRequestParameter("asdf").isPresent());
+  }
+
+  @Test
+  public void test_getRequestParameter() {
+    HttpServletRequest httpRequestMock = expectRequest();
+    expect(httpRequestMock.getParameter("asdf")).andReturn("val").anyTimes();
+    replayDefault();
+    Optional<String> ret = modelContext.getRequestParameter("asdf");
+    verifyDefault();
+    assertTrue(ret.isPresent());
+    assertEquals("val", ret.get());
+  }
+
+  private HttpServletRequest expectRequest() {
+    HttpServletRequest httpRequestMock = createMockAndAddToDefault(HttpServletRequest.class);
+    getContext().setRequest(new XWikiServletRequest(httpRequestMock));
+    expect(httpRequestMock.getCharacterEncoding()).andReturn("").anyTimes();
+    return httpRequestMock;
   }
 
   private void assertDefaultContext() {
