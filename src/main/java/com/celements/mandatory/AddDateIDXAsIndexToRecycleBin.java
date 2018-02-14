49ec5fefaafc85fe79645e3c45506c1810799b08
Dari Khasham <dari.khasham@synventis.com>
2018-02-14 14:17:57 +0100

    alter table xwikirecyclebin

--- src/main/java/com/celements/mandatory/AddDateIDXAsIndexToRecycleBin.java
+++ src/main/java/com/celements/mandatory/AddDateIDXAsIndexToRecycleBin.java
@@ -22,28 +22,23 @@ public class AddDateIDXAsIndexToRecycleBin implements IMandatoryDocumentRole {
 
   @Override
   public List<String> dependsOnMandatoryDocuments() {
-    System.out.println(
-        "<<<<<<<<<<<<<<<< AddDateIDXAsIndexToRecycleBin dependsOnMandatoryDocuments");
     return Collections.emptyList();
   }
 
   @Override
   public void checkDocuments() throws XWikiException {
-    System.out.println("<<<<<<<<<<<<<<<< AddDateIDXAsIndexToRecycleBin checkDocuments");
     LOGGER.info("executing AddDateIDXAsIndexToRecycleBin");
     // FIXME make sure that these operations are ONLY executed ONCE on any DB.
     queryExecService.executeWriteSQLs(getIndexSQLs());
   }
 
   private List<String> getIndexSQLs() {
-    System.out.println("<<<<<<<<<<<<<<<< AddDateIDXAsIndexToRecycleBin getIndexSQLs");
     List<String> ret = new ArrayList<String>();
     ret.add(getSQLIndexToRecycleBin());
     return ret;
   }
 
   String getSQLIndexToRecycleBin() {
-    System.out.println("<<<<<<<<<<<<<<<< AddDateIDXAsIndexToRecycleBin getSQLIndexToRecycleBin");
     return "alter table xwikirecyclebin add index `dateIDX` (XDD_DATE, XDD_ID);";
   }
 
