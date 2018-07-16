9a608e8e158818769a2f614a6c9f6bdb58aba931
Marc Sladek <marc.sladek@synventis.com>
2018-07-16 14:56:48 +0200

    add AuthClassPackage

---
+++ src/main/java/com/celements/auth/classes/AuthClassPackage.java
@@ -0,0 +1,30 @@
+package com.celements.auth.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.annotation.Requirement;
+
+import com.celements.model.classes.AbstractClassPackage;
+import com.celements.model.classes.ClassDefinition;
+
+@Component(AuthClassPackage.NAME)
+public class AuthClassPackage extends AbstractClassPackage {
+
+  public static final String NAME = "auth";
+
+  @Requirement
+  private List<AuthClass> classDefs;
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @Override
+  public List<? extends ClassDefinition> getClassDefinitions() {
+    return new ArrayList<>(classDefs);
+  }
+
+}
