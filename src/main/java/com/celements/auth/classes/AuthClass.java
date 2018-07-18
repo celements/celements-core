1f4d4dd86ee629a1b7b851bbc970e74d30d63b38
Marc Sladek <marc.sladek@synventis.com>
2018-07-18 18:38:28 +0200

    RemoteLoginClass (#178)
    * update dependencies
    
    * add AuthClassPackage
    
    * add RemoteLoginClass

---
+++ src/main/java/com/celements/auth/classes/AuthClass.java
@@ -0,0 +1,12 @@
+package com.celements.auth.classes;
+
+import org.xwiki.component.annotation.ComponentRole;
+
+import com.celements.model.classes.ClassDefinition;
+
+@ComponentRole
+public interface AuthClass extends ClassDefinition {
+
+  public static final String CLASS_SPACE = "Classes";
+
+}
