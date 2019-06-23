9b9c383826248bd89b936ca1dd5c41a4e11c6ebf
pichlerf <fabian.pichler@synventis.com>
2019-06-23 19:03:50 +0200

    implement rteConfig implementation selection and TinyMce4Config

--- src/main/java/com/celements/rteConfig/RTEConfigScriptService.java
+++ src/main/java/com/celements/rteConfig/RTEConfigScriptService.java
@@ -21,46 +21,75 @@ package com.celements.rteConfig;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
+import org.xwiki.context.Execution;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.script.service.ScriptService;
 
 import com.celements.emptycheck.internal.IDefaultEmptyDocStrategyRole;
+import com.google.common.base.MoreObjects;
 
 @Component("rteconfig")
 public class RTEConfigScriptService implements ScriptService {
 
-  @Requirement
-  IDefaultEmptyDocStrategyRole defaultEmptyDocStrategyRole;
+  private static final String RTE_CONFIG_ACTIV_HINT = "RTE_CONFIG_ACTIV_HINT";
 
   @Requirement
-  RteConfigRole rteConfig;
+  private IDefaultEmptyDocStrategyRole defaultEmptyDocStrategyRole;
+
+  @Requirement
+  private Map<String, RteConfigRole> rteConfigMap;
+
+  @Requirement
+  private Execution execution;
 
   private static Logger LOGGER = LoggerFactory.getLogger(RTEConfigScriptService.class);
 
-  public String getRTEConfigField(String name) {
+  @NotNull
+  public String getRTEConfigField(@NotNull String name) {
     try {
-      return rteConfig.getRTEConfigField(name);
+      return getActiveRteConfig().getRTEConfigField(name);
     } catch (Exception exp) {
       LOGGER.error("getRTEConfigField for name [" + name + "] failed.", exp);
     }
     return "";
   }
 
-  public boolean isEmptyRTEString(String rteContent) {
+  public boolean isEmptyRTEString(@NotNull String rteContent) {
     return defaultEmptyDocStrategyRole.isEmptyRTEString(rteContent);
   }
 
+  @NotNull
   public List<DocumentReference> getRTEConfigsList() {
     try {
-      return rteConfig.getRTEConfigsList();
+      return getActiveRteConfig().getRTEConfigsList();
     } catch (Exception exp) {
       LOGGER.error("getRTEConfigsList failed.", exp);
     }
     return Collections.emptyList();
   }
+
+  public void setRteConfigHint(@Nullable String rteConfigHint) {
+    execution.getContext().setProperty(RTE_CONFIG_ACTIV_HINT, rteConfigHint);
+  }
+
+  @NotNull
+  public String getRteConfigHint() {
+    return (String) MoreObjects.firstNonNull(execution.getContext().getProperty(
+        RTE_CONFIG_ACTIV_HINT), "default");
+  }
+
+  @Nullable
+  private RteConfigRole getActiveRteConfig() {
+    return rteConfigMap.get(getRteConfigHint());
+  }
+
 }
