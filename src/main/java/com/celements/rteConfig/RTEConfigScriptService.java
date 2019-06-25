93addf6f2ab85de0ea45e059eda4739eb91f071f
Fabian <fabian.pichler@synventis.com>
2019-06-25 17:17:10 +0200

    CELDEV-831 refactoring RTEConfig prepareing extending (#215)
    * refactor RTEConfig to a component and replace deprecated calls
    
    * refactor RTEConfig
    
    * refactor RTEConfigTest
    
    * add license comment
    
    * add deprecated-since note
    
    * remove unused old XWikiException in signature.
    
    * add NotNull annotations
    
    * CELDEV-831- Extend TinyConfig4 config for row-config (#216)
    
    * remove deprecated unused RTEConfig.java elements
    
    * implement rteConfig implementation selection and TinyMce4Config
    
    * implement rteConfig implementation selection and TinyMce4Config
    
    * add CAUTION comment to TinyMce4Config HINT
    
    * move TinyMCE4Config to struct editor
    
    * move TinyMce4Config to struct editor

--- src/main/java/com/celements/rteConfig/RTEConfigScriptService.java
+++ src/main/java/com/celements/rteConfig/RTEConfigScriptService.java
@@ -1,39 +1,95 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package com.celements.rteConfig;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
+import org.xwiki.context.Execution;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.script.service.ScriptService;
 
 import com.celements.emptycheck.internal.IDefaultEmptyDocStrategyRole;
-import com.xpn.xwiki.XWikiException;
+import com.google.common.base.MoreObjects;
 
 @Component("rteconfig")
 public class RTEConfigScriptService implements ScriptService {
 
+  private static final String RTE_CONFIG_ACTIV_HINT = "RTE_CONFIG_ACTIV_HINT";
+
   @Requirement
-  IDefaultEmptyDocStrategyRole defaultEmptyDocStrategyRole;
+  private IDefaultEmptyDocStrategyRole defaultEmptyDocStrategyRole;
 
-  private static Logger _LOGGER = LoggerFactory.getLogger(RTEConfigScriptService.class);
+  @Requirement
+  private Map<String, RteConfigRole> rteConfigMap;
 
-  public String getRTEConfigField(String name) {
+  @Requirement
+  private Execution execution;
+
+  private static Logger LOGGER = LoggerFactory.getLogger(RTEConfigScriptService.class);
+
+  @NotNull
+  public String getRTEConfigField(@NotNull String name) {
     try {
-      return new RTEConfig().getRTEConfigField(name);
-    } catch (XWikiException exp) {
-      _LOGGER.error("getRTEConfigField for name [" + name + "] failed.", exp);
+      return getActiveRteConfig().getRTEConfigField(name);
+    } catch (Exception exp) {
+      LOGGER.error("getRTEConfigField for name [" + name + "] failed.", exp);
     }
     return "";
   }
 
-  public boolean isEmptyRTEString(String rteContent) {
+  public boolean isEmptyRTEString(@NotNull String rteContent) {
     return defaultEmptyDocStrategyRole.isEmptyRTEString(rteContent);
   }
 
+  @NotNull
   public List<DocumentReference> getRTEConfigsList() {
-    return new RTEConfig().getRTEConfigsList();
+    try {
+      return getActiveRteConfig().getRTEConfigsList();
+    } catch (Exception exp) {
+      LOGGER.error("getRTEConfigsList failed.", exp);
+    }
+    return Collections.emptyList();
   }
+
+  public void setRteConfigHint(@Nullable String rteConfigHint) {
+    execution.getContext().setProperty(RTE_CONFIG_ACTIV_HINT, rteConfigHint);
+  }
+
+  @NotNull
+  public String getRteConfigHint() {
+    return (String) MoreObjects.firstNonNull(execution.getContext().getProperty(
+        RTE_CONFIG_ACTIV_HINT), "default");
+  }
+
+  @Nullable
+  private RteConfigRole getActiveRteConfig() {
+    return rteConfigMap.get(getRteConfigHint());
+  }
+
 }
