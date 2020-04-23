30a941925e3d130f3ab093dee106bae9cd532f42
Marc Sladek <marc@sladek.me>
2020-04-23 20:42:02 +0200

    Release 4.5 (#246)
    * lined concat method for debug
    
    * wrap concat
    
    * add javadoc
    
    * update dependencies
    
    * [maven-release-plugin] prepare release celements-core-4.5
    
    * [maven-release-plugin] prepare for next development iteration
    
    Co-authored-by: CI synventis <jenkins@synventis.com>

--- src/main/java/com/celements/collections/service/StreamScriptService.java
+++ src/main/java/com/celements/collections/service/StreamScriptService.java
@@ -22,7 +22,15 @@ public class StreamScriptService implements ScriptService {
   }
 
   public Stream<?> concat(Stream<?>... streams) {
-    return Stream.of(streams).map(this::nullGuard).reduce(Stream::concat).orElseGet(Stream::empty);
+    return Stream.of(streams).map(this::nullGuard).reduce(this::concat).orElseGet(Stream::empty);
+  }
+
+  /**
+   * wrap concat since it's generic parameters <? extends T>
+   * causes compile errors in older VMs when used with <?>
+   */
+  private <T> Stream<T> concat(Stream<T> s1, Stream<T> s2) {
+    return Stream.concat(s1, s2);
   }
 
   public <T> List<T> toList(Stream<T> stream) {
