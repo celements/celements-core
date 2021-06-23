b3d8188fee93810653d16eb87b9b575e1d6c925e
Marc Sladek <marc@sladek.me>
2021-06-23 16:28:21 +0200

    TimeZoneInit fix component usage (#282)

--- src/main/java/com/celements/init/TimeZoneInit.java
+++ src/main/java/com/celements/init/TimeZoneInit.java
@@ -2,23 +2,27 @@ package com.celements.init;
 
 import java.time.DateTimeException;
 import java.time.ZoneId;
-import java.util.Arrays;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
+import org.xwiki.component.annotation.Requirement;
+import org.xwiki.configuration.ConfigurationSource;
 import org.xwiki.observation.event.ApplicationStartedEvent;
 import org.xwiki.observation.event.Event;
 
 import com.celements.common.date.DateUtil;
 import com.celements.common.observation.listener.AbstractLocalEventListener;
+import com.celements.configuration.CelementsAllPropertiesConfigurationSource;
 import com.celements.configuration.ConfigSourceUtils;
+import com.google.common.collect.ImmutableList;
 
 @Component(TimeZoneInit.NAME)
 public class TimeZoneInit extends AbstractLocalEventListener<Object, Object> {
 
   public static final String NAME = "TimeZoneInit";
+
+  @Requirement(CelementsAllPropertiesConfigurationSource.NAME)
+  private ConfigurationSource cfgSrc;
 
   @Override
   public String getName() {
@@ -27,19 +31,20 @@ public class TimeZoneInit extends AbstractLocalEventListener<Object, Object> {
 
   @Override
   public List<Event> getEvents() {
-    return Arrays.asList(new ApplicationStartedEvent());
+    return ImmutableList.of(new ApplicationStartedEvent());
   }
 
   @Override
   protected void onEventInternal(Event event, Object source, Object data) {
-    ConfigSourceUtils.getStringProperty("celements.time.zone").toJavaUtil().ifPresent(zone -> {
-      try {
-        DateUtil.setDefaultZone(ZoneId.of(zone));
-        LOGGER.info("init time zone [{}]", zone);
-      } catch (DateTimeException exc) {
-        LOGGER.warn("failed to init time zone [{}]", zone, exc);
-      }
-    });
+    ConfigSourceUtils.getStringProperty(cfgSrc, "celements.time.zone").toJavaUtil()
+        .ifPresent(zone -> {
+          try {
+            DateUtil.setDefaultZone(ZoneId.of(zone));
+            LOGGER.info("init time zone [{}]", zone);
+          } catch (DateTimeException exc) {
+            LOGGER.warn("failed to init time zone [{}]", zone, exc);
+          }
+        });
   }
 
 }
