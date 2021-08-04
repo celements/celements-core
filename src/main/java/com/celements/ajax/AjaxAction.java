7ee57359831edb64ce9ae2b5adf4f2c86a1db47f
Fabian Pichler <fabian.pichler@synventis.com>
2021-08-05 00:12:11 +0200

    compute path

--- src/main/java/com/celements/ajax/AjaxAction.java
+++ src/main/java/com/celements/ajax/AjaxAction.java
@@ -1,10 +1,15 @@
 package com.celements.ajax;
 
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.apache.velocity.VelocityContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.celements.appScript.IAppScriptService;
+import com.google.common.base.Joiner;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.web.Utils;
@@ -13,7 +18,7 @@ import com.xpn.xwiki.web.XWikiAction;
 public class AjaxAction extends XWikiAction {
 
   private static final String CEL_AJAX_CONTEXT_PROPERTY = "celAjax";
-  private static final String ACTION_SCRIPT_ACTION = "ajax";
+  private static final String ACTION_SCRIPT_ACTION = "ajax_mode";
 
   private static final Logger LOGGER = LoggerFactory.getLogger(AjaxAction.class);
 
@@ -33,9 +38,11 @@ public class AjaxAction extends XWikiAction {
     context.put("action", VIEW_ACTION);
     context.put("ajaxAction", true);
     String path = context.getRequest().getPathInfo();
-    if (getAppScriptService().getStartIndex(path) > 0) {
-      String celAjaxScript = getAppScriptService().getScriptNameFromURL();
-      LOGGER.error("ajax: found script path '{}'.", celAjaxScript);
+    List<String> pathParts = Stream.of(path.split("/"))
+        .filter(((Predicate<String>) String::isEmpty).negate()).collect(Collectors.toList());
+    if (pathParts.size() > 2) {
+      String celAjaxScript = Joiner.on("/").join(pathParts);
+      LOGGER.error("ajax: found script path '{}' from path '{}'.", celAjaxScript, path);
       context.put(CEL_AJAX_CONTEXT_PROPERTY, celAjaxScript);
       VelocityContext vcontext = (VelocityContext) context.get("vcontext");
       vcontext.put(CEL_AJAX_CONTEXT_PROPERTY, celAjaxScript);
@@ -54,10 +61,6 @@ public class AjaxAction extends XWikiAction {
     LOGGER.error("Ajax: render page '{}'", page);
     Utils.parseTemplate(page, !page.equals("direct"), context);
     return null;
-  }
-
-  private IAppScriptService getAppScriptService() {
-    return Utils.getComponent(IAppScriptService.class);
   }
 
 }
