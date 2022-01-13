b5ea978386086cf52755218d635eb3c6e72e829a
Fabian Pichler <fabian.pichler@synventis.com>
2022-01-13 16:00:22 +0100

    add logging to convertStoreValue

--- src/main/java/com/celements/javascript/JsLoadMode.java
+++ src/main/java/com/celements/javascript/JsLoadMode.java
@@ -3,17 +3,23 @@ package com.celements.javascript;
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.base.Strings;
 
 public enum JsLoadMode {
 
   SYNC, DEFER, ASYNC;
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(JsLoadMode.class);
+
   @NotNull
   public static JsLoadMode convertStoreValue(@Nullable String storeValue) {
     try {
       return valueOf(Strings.nullToEmpty(storeValue).toUpperCase());
     } catch (IllegalArgumentException exp) {
+      LOGGER.info("JsLoadMode convertStoreValue failed for [{}] using SYNC", storeValue);
       return SYNC;
     }
   }
