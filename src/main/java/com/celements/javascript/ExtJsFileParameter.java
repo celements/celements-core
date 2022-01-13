5c5cd7f1d3a4c04a91eeb9cff46920e0c699e774
Fabian Pichler <fabian.pichler@synventis.com>
2022-01-13 12:44:55 +0100

    add debug log

--- src/main/java/com/celements/javascript/ExtJsFileParameter.java
+++ src/main/java/com/celements/javascript/ExtJsFileParameter.java
@@ -3,38 +3,46 @@ package com.celements.javascript;
 import static com.google.common.base.Preconditions.*;
 
 import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
 import javax.validation.constraints.NotNull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.celements.web.plugin.cmd.AttachmentURLCommand;
 
+@NotThreadSafe
 public final class ExtJsFileParameter {
 
-  @NotNull
+  private static final Logger LOGGER = LoggerFactory.getLogger(ExtJsFileParameter.class);
+
   private String jsFile;
-  @Nullable
-  private String action = null;
-  @Nullable
-  private String params = null;
-  @Nullable
-  private AttachmentURLCommand attUrlCmd = null;
+  private String action;
+  private String params;
+  private AttachmentURLCommand attUrlCmd;
   private JsLoadMode loadMode = JsLoadMode.SYNC;
 
+  @Nullable
   public String getJsFile() {
     return jsFile;
   }
 
+  @Nullable
   public String getAction() {
     return action;
   }
 
+  @Nullable
   public String getParams() {
     return params;
   }
 
+  @Nullable
   public AttachmentURLCommand getAttUrlCmd() {
     return attUrlCmd;
   }
 
+  @Nullable
   public JsLoadMode getLoadMode() {
     return loadMode;
   }
@@ -61,6 +69,7 @@ public final class ExtJsFileParameter {
   }
 
   public ExtJsFileParameter setLoadMode(JsLoadMode loadMode) {
+    LOGGER.debug("setLoadMode: [{}] for js file [{}].", loadMode, jsFile);
     this.loadMode = loadMode;
     return this;
   }
