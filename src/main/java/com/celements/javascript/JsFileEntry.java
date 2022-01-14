34ab8c9aa82e9f14bb5d8710ee5488466ec11891
Fabian Pichler <fabian.pichler@synventis.com>
2022-01-14 04:04:08 +0100

    add missing properties to JsFileEntry

--- src/main/java/com/celements/javascript/JsFileEntry.java
+++ src/main/java/com/celements/javascript/JsFileEntry.java
@@ -3,14 +3,23 @@ package com.celements.javascript;
 import java.util.Objects;
 import java.util.Optional;
 
+import org.xwiki.model.reference.ClassReference;
+import org.xwiki.model.reference.DocumentReference;
+
 import com.google.common.base.Strings;
 
 public final class JsFileEntry {
 
+  private int id;
+  private DocumentReference documentReference;
   private int objNumber;
-
+  private ClassReference classReference;
   private String jsFileUrl;
   private JsLoadMode loadMode;
+
+  public JsFileEntry() {
+    // Bean needs default constructor
+  }
 
   public JsFileEntry addFilepath(String jsFile) {
     setFilepath(jsFile);
@@ -44,6 +53,30 @@ public final class JsFileEntry {
 
   public void setNumber(int objNumber) {
     this.objNumber = objNumber;
+  }
+
+  public ClassReference getClassReference() {
+    return classReference;
+  }
+
+  public void setClassReference(ClassReference classReference) {
+    this.classReference = classReference;
+  }
+
+  public DocumentReference getDocumentReference() {
+    return documentReference;
+  }
+
+  public void setDocumentReference(DocumentReference documentReference) {
+    this.documentReference = documentReference;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public boolean isValid() {
