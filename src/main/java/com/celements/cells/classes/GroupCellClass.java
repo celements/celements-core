8ddd8bb9ec58d521fc5a389c51cb7a3c44837986
Fabian Pichler <fabian.pichler@synventis.com>
2022-01-20 16:22:38 +0100

    add GroupCellClass class definition

---
+++ src/main/java/com/celements/cells/classes/GroupCellClass.java
@@ -0,0 +1,39 @@
+package com.celements.cells.classes;
+
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.ClassReference;
+
+import com.celements.model.classes.AbstractClassDefinition;
+import com.celements.model.classes.fields.ClassField;
+import com.celements.model.classes.fields.StringField;
+import com.google.errorprone.annotations.Immutable;
+
+@Singleton
+@Immutable
+@Component(GroupCellClass.CLASS_DEF_HINT)
+public class GroupCellClass extends AbstractClassDefinition
+    implements CellsClassDefinition {
+
+  public static final String DOC_NAME = "GroupCellClass";
+  private static final String SPACE_NAME = "Celements";
+  public static final String CLASS_DEF_HINT = SPACE_NAME + "." + DOC_NAME;
+  public static final ClassReference CLASS_REF = new ClassReference(SPACE_NAME, DOC_NAME);
+
+  public static final String EDITOR_LAYOUT_VALUE = "editorLayout";
+  public static final String PAGE_LAYOUT_VALUE = "pageLayout";
+
+  public static final ClassField<String> FIELD_RENDER_LAYOUT = new StringField.Builder(CLASS_REF,
+      "render_layout").prettyName("Render Layout").size(30).build();
+
+  public GroupCellClass() {
+    super(CLASS_REF);
+  }
+
+  @Override
+  public boolean isInternalMapping() {
+    return false;
+  }
+
+}
