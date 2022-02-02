7cb81bf44657666411de454f52a941f4e147a2d5
Fabian Pichler <fabian.pichler@synventis.com>
2022-02-02 11:27:49 +0100

    comment unfinished method

--- src/main/java/com/celements/javascript/service/JavaScriptService.java
+++ src/main/java/com/celements/javascript/service/JavaScriptService.java
@@ -1,14 +1,28 @@
 package com.celements.javascript.service;
 
-import java.util.Map;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Requirement;
 
-import com.celements.rendering.head.HtmlHeadConfiguratorRole;
+import com.celements.pagelayout.LayoutServiceRole;
+import com.celements.ressource_url.RessourceUrlServiceRole;
 
 public class JavaScriptService implements JavaScriptServiceRole {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(JavaScriptService.class);
+
   @Requirement
-  private Map<String, HtmlHeadConfiguratorRole> htmlHeadConfiguratorMap;
+  private LayoutServiceRole layoutService;
+
+  @Requirement
+  private RessourceUrlServiceRole attUrlService;
+
+  public String getAllExternalJavaScriptFiles() {
+    // return layoutService.getHtmlHeadConfigurator(layoutService.getCurrentRenderingLayout())
+    // .getAllInitialJavaScriptFiles().stream()
+    // .map(this::generateScriptTag)
+    // .forEach();
+    return "";
+  }
 
 }
