3c5ed77240b5a01859a5121ed80c7dd155396615
pichlerf <fabian.pichler@synventis.com>
2022-09-23 18:54:54 +0200

    add missing surrounding div

--- src/main/java/com/celements/navigation/presentation/RenderedContentDynLoadPresentationType.java
+++ src/main/java/com/celements/navigation/presentation/RenderedContentDynLoadPresentationType.java
@@ -1,5 +1,7 @@
 package com.celements.navigation.presentation;
 
+import javax.validation.constraints.NotNull;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
@@ -23,14 +25,20 @@ public class RenderedContentDynLoadPresentationType extends RenderedContentPrese
   public void writeNodeContent(StringBuilder outStream, boolean isFirstItem, boolean isLastItem,
       DocumentReference docRef, boolean isLeaf, int numItem, INavigation nav) {
     LOGGER.debug("writeNodeContent for [{}].", docRef);
-    outStream.append("<cel-lazy-load ");
+    outStream.append("<div ");
     outStream.append(nav.addCssClasses(docRef, true, isFirstItem, isLastItem, isLeaf, numItem)
         + " ");
-    String loadSrc = urlSrv.getExternalURL(docRef, "view",
-        "xpage=ajax&ajax_mode=pageTypeWithLayout&ajax=1&overwriteLayout=SimpleLayout");
-    outStream.append("src=\"" + loadSrc + "\" size=32 ");
     outStream.append(nav.addUniqueElementId(docRef) + ">\n");
+    outStream.append("<cel-lazy-load ");
+    String loadSrc = getLoadSrcUrl(docRef);
+    outStream.append("src=\"" + loadSrc + "\" size=32 ");
     outStream.append("</cel-lazy-load>\n");
+    outStream.append("</div>\n");
+  }
+
+  private @NotNull String getLoadSrcUrl(DocumentReference docRef) {
+    return urlSrv.getURL(docRef, "view",
+        "xpage=ajax&ajax_mode=pageTypeWithLayout&ajax=1&overwriteLayout=SimpleLayout");
   }
 
 }
