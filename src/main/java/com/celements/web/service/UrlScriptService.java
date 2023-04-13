b065d5ae9cf3a86c55f8a48ed48026633b3f4871
Marc Sladek <marc@sladek.dev>
2023-04-13 02:03:50 +0200

    UrlScriptService catch exceptions

--- src/main/java/com/celements/web/service/UrlScriptService.java
+++ src/main/java/com/celements/web/service/UrlScriptService.java
@@ -4,6 +4,8 @@ import static com.google.common.base.MoreObjects.*;
 
 import javax.ws.rs.core.UriBuilder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
 import org.xwiki.model.reference.EntityReference;
@@ -14,6 +16,8 @@ import com.celements.model.context.ModelContext;
 @Component("url")
 public class UrlScriptService implements ScriptService {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(UrlScriptService.class);
+
   @Requirement
   private UrlService urlService;
 
@@ -21,55 +25,69 @@ public class UrlScriptService implements ScriptService {
   private ModelContext context;
 
   public String getURL() {
-    return urlService.getURL(getCurrentReference());
+    return getURL(null);
   }
 
   public String getURL(EntityReference ref) {
-    return urlService.getURL(firstNonNull(ref, getCurrentReference()));
+    return getURL(ref, null);
   }
 
   public String getURL(EntityReference ref, String action) {
-    return urlService.getURL(firstNonNull(ref, getCurrentReference()), action);
+    return getURL(ref, action, null);
   }
 
   public String getURL(EntityReference ref, String action, String queryString) {
-    return urlService.getURL(firstNonNull(ref, getCurrentReference()), action, queryString);
+    try {
+      return urlService.getURL(firstNonNull(ref, getCurrentReference()), action, queryString);
+    } catch (Exception iae) {
+      LOGGER.debug("getURL - failed for [{}], [{}], [{}]", ref, action, queryString);
+      return null;
+    }
   }
 
   public String getExternalURL() {
-    return urlService.getExternalURL(getCurrentReference());
+    return getExternalURL(null);
   }
 
   public String getExternalURL(EntityReference ref) {
-    return urlService.getExternalURL(firstNonNull(ref, getCurrentReference()));
+    return getExternalURL(ref, null);
   }
 
   public String getExternalURL(EntityReference ref, String action) {
-    return urlService.getExternalURL(firstNonNull(ref, getCurrentReference()), action);
+    return getExternalURL(ref, action, null);
   }
 
   public String getExternalURL(EntityReference ref, String action, String queryString) {
-    return urlService.getExternalURL(firstNonNull(ref, getCurrentReference()), action, queryString);
+    try {
+      return urlService.getExternalURL(firstNonNull(ref, getCurrentReference()), action,
+          queryString);
+    } catch (Exception iae) {
+      LOGGER.debug("getExternalURL - failed for [{}], [{}], [{}]", ref, action, queryString);
+      return null;
+    }
   }
 
   public UriBuilder createURIBuilder() {
-    return urlService.createURIBuilder(getCurrentReference());
+    return createURIBuilder(null);
   }
 
   UriBuilder createURIBuilder(EntityReference ref) {
-    return urlService.createURIBuilder(firstNonNull(ref, getCurrentReference()));
+    return createURIBuilder(ref, null);
   }
 
   UriBuilder createURIBuilder(EntityReference ref, String action) {
-    return urlService.createURIBuilder(firstNonNull(ref, getCurrentReference()), action);
+    try {
+      return urlService.createURIBuilder(firstNonNull(ref, getCurrentReference()), action);
+    } catch (Exception iae) {
+      LOGGER.debug("createURIBuilder - failed for [{}], [{}]", ref, action);
+      return null;
+    }
   }
 
   private EntityReference getCurrentReference() {
-    if (context.getDoc() != null) {
-      return context.getDoc().getDocumentReference();
-    } else {
-      return context.getWikiRef();
-    }
+    return context.getDocRef()
+        .map(EntityReference.class::cast)
+        .orElseGet(context::getWikiRef);
   }
 
 }
