dbda81066e3a78f6ed8ef4df24a1d726a84733cc
Chantal Pichler <chantal.pichler@synventis.com>
2023-05-31 15:39:47 +0200

    add UserPageType and UserPageTypeTest

---
+++ src/main/java/com/celements/auth/user/UserPageType.java
@@ -0,0 +1,73 @@
+package com.celements.auth.user;
+
+import java.util.Set;
+
+import javax.validation.constraints.NotNull;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.annotation.Requirement;
+
+import com.celements.pagetype.category.IPageTypeCategoryRole;
+import com.celements.pagetype.java.AbstractJavaPageType;
+import com.google.common.collect.Sets;
+
+@Component(UserPageType.PAGETYPE_NAME)
+public class UserPageType extends AbstractJavaPageType {
+
+  public static final String PAGETYPE_NAME = "User";
+
+  static final String VIEW_TEMPLATE_NAME = "UserView";
+
+  static final String EDIT_TEMPLATE_NAME = "UserEdit";
+
+  @Requirement
+  private IPageTypeCategoryRole pageTypeCategory;
+
+  @Override
+  public String getName() {
+    return PAGETYPE_NAME;
+  }
+
+  @Override
+  public boolean displayInFrameLayout() {
+    return true;
+  }
+
+  @Override
+  public Set<IPageTypeCategoryRole> getCategories() {
+    return Sets.newHashSet(pageTypeCategory);
+  }
+
+  @Override
+  public boolean hasPageTitle() {
+    return false;
+  }
+
+  @Override
+  public boolean isUnconnectedParent() {
+    return false;
+  }
+
+  @Override
+  public boolean isVisible() {
+    return true;
+  }
+
+  String getViewTemplateName() {
+    return VIEW_TEMPLATE_NAME;
+  }
+
+  String getEditTemplateName() {
+    return EDIT_TEMPLATE_NAME;
+  }
+
+  @Override
+  public @NotNull String getRenderTemplateForRenderMode(String renderMode) {
+    if ("edit".equals(renderMode)) {
+      return getEditTemplateName();
+    } else {
+      return getViewTemplateName();
+    }
+  }
+
+}
