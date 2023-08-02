ae0866b9199778cdceeb681d48bed9d3c317020b
Chantal Pichler <chantal.pichler@synventis.com>
2023-08-02 17:08:12 +0200

    rename the Listener and finish it

--- src/main/java/com/celements/auth/user/listener/AddDefaultGroupsToNewUserListener.java
+++ src/main/java/com/celements/auth/user/listener/AddDefaultGroupsToNewUserListener.java
@@ -1,15 +1,12 @@
 package com.celements.auth.user.listener;
 
-import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.xwiki.bridge.event.DocumentCreatedEvent;
-import org.xwiki.model.reference.ClassReference;
 import org.xwiki.observation.event.Event;
 
 import com.celements.auth.user.CelementsUserService;
@@ -17,21 +14,18 @@ import com.celements.auth.user.User;
 import com.celements.auth.user.UserInstantiationException;
 import com.celements.common.observation.listener.AbstractLocalEventListener;
 import com.celements.model.access.exception.DocumentSaveException;
-import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.doc.XWikiDocument;
 
-public class AddXWikiAllGroupToNewUserListener
-    extends AbstractLocalEventListener<XWikiDocument, XWikiContext> {
+@Component
+public class AddDefaultGroupsToNewUserListener
+    extends AbstractLocalEventListener<XWikiDocument, Object> {
 
-  private static final Logger LOGGER = LoggerFactory
-      .getLogger(AddXWikiAllGroupToNewUserListener.class);
-
-  private static final String NAME = "addXWikiAllGroupToNewUser";
+  private static final String NAME = "addDefaultGroupsToNewUser";
 
   private final CelementsUserService celUserService;
 
   @Inject
-  public AddXWikiAllGroupToNewUserListener(CelementsUserService celUserService) {
+  public AddDefaultGroupsToNewUserListener(CelementsUserService celUserService) {
     super();
     this.celUserService = celUserService;
   }
@@ -43,23 +37,18 @@ public class AddXWikiAllGroupToNewUserListener
 
   @Override
   public List<Event> getEvents() {
-    return Arrays.<Event>asList(new DocumentCreatedEvent());
+    return List.of(new DocumentCreatedEvent());
   }
 
   @Override
-  protected void onEventInternal(@NotNull Event event, XWikiDocument source, XWikiContext data) {
-    LOGGER.trace("onEvent in addXWikiAllGroupToNewUser for source [{}] and context [{}].", source,
+  protected void onEventInternal(@NotNull Event event, XWikiDocument source, Object data) {
+    LOGGER.trace("onEvent in addDefaultGroupsToNewUser for source [{}] and context [{}].", source,
         context);
     try {
       User user = celUserService.getUser(source.getDocRef());
-      // Dokument zur XWikiAllGroup hinzufügen (celUserService.addUserToDefaultGroups wäre perfekt,
-      // ist aber nicht public.)
-      ClassReference groupRef = null;
-      try {
-        celUserService.addUserToGroup(user, groupRef);
-      } catch (DocumentSaveException dse) {
-        LOGGER.debug("source {} couldn't be added to groupRef {}", source, groupRef, dse);
-      }
+      celUserService.addUserToDefaultGroups(user);
+    } catch (DocumentSaveException dse) {
+      LOGGER.error("source {} couldn't be added to defaultGroups", source, dse);
     } catch (UserInstantiationException uie) {
       LOGGER.debug("source {} is no user document", source, uie);
     }
