0a2f651e38aff4536505f55fdc9cef421f52158a
Chantal Pichler <chantal.pichler@synventis.com>
2023-09-11 15:27:47 +0200

    rename GroupServiceTest and add more empty Tests

---
+++ src/test/java/com/celements/auth/groups/GroupServiceTest.java
@@ -0,0 +1,50 @@
+package com.celements.auth.groups;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.celements.common.test.AbstractComponentTest;
+
+public class GroupServiceTest extends AbstractComponentTest {
+
+  @Before
+  public void prepare_Test() {
+
+  }
+
+  @Test
+  public void test_getAllGroups() {
+
+  }
+
+  @Test
+  public void test_getAllGroups_wikiRefNull() {
+
+  }
+
+  @Test
+  public void test_getAllGroups_failing() {
+
+  }
+
+  @Test
+  public void test_getGroupPrettyName() {
+
+  }
+
+  @Test
+  public void test_getGroupPrettyName_docRefNull() {
+
+  }
+
+  @Test
+  public void test_getGroupPrettyName_noDictKey() {
+
+  }
+
+  @Test
+  public void test_getGroupPrettyName_noDocTitle() {
+
+  }
+
+}
