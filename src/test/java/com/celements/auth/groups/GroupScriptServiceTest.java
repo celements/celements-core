ae4440ce2b0f1a3c5ed5917b4decb118fc8d6500
Chantal Pichler <chantal.pichler@synventis.com>
2023-09-13 17:04:32 +0200

    prepare GroupScriptServiceTest

---
+++ src/test/java/com/celements/auth/groups/GroupScriptServiceTest.java
@@ -0,0 +1,32 @@
+package com.celements.auth.groups;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.celements.common.test.AbstractComponentTest;
+
+public class GroupScriptServiceTest extends AbstractComponentTest {
+
+  private GroupScriptService service;
+
+  @Before
+  public void prepare_Test() {
+
+  }
+
+  @Test
+  public void test_getGroupPrettyName() {
+
+  }
+
+  @Test
+  public void test_getGroupPrettyName_emptyOptional() {
+
+  }
+
+  @Test
+  public void test_getGroupPrettyName_OptionalWithEmptyString() {
+
+  }
+
+}
