8c622e3ce2749b4444e384f96a5df2d06ca556cb
Fabian Pichler <fabian.pichler@synventis.com>
2024-07-14 01:50:22 +0200

    add rest LayoutController

---
+++ src/main/java/com/celements/pagelayout/rest/RenderPartialRequest.java
@@ -0,0 +1,10 @@
+package com.celements.pagelayout.rest;
+
+public class RenderPartialRequest {
+
+  public String contextDocSpace;
+  public String contextDocName;
+  public String layoutSpace;
+  public String startNodeName;
+
+}
