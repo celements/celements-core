53c5b6bee4ea9fb31176219a8415007f92e65b09
Fabian Pichler <fabian.pichler@synventis.com>
2025-03-09 21:32:01 +0100

    change replaceAll to replace if no regex used

--- src/main/java/com/celements/web/service/WebUtilsService.java
+++ src/main/java/com/celements/web/service/WebUtilsService.java
@@ -1135,10 +1135,10 @@ public class WebUtilsService implements IWebUtilsService {
             && getModelAccess().exists(getCentralTemplateRef(localTemplateRef))) {
           templatePath = "celements2web:" + templatePath;
         } else {
-          templatePath = ":" + templatePath.replaceAll("celements2web:", "");
+          templatePath = ":" + templatePath.replace("celements2web:", "");
         }
       }
-      return templatePath.replaceAll(getContext().getDatabase() + ":", "");
+      return templatePath.replace(getContext().getDatabase() + ":", "");
     }
     return null;
   }
@@ -1208,8 +1208,8 @@ public class WebUtilsService implements IWebUtilsService {
       renderCommand.setRenderingEngine(this.injectedRenderingEngine);
     }
     String templatePath = getInheritedTemplatedPath(docRef);
-    LOGGER.debug("renderInheritableDocument: call renderTemplatePath for [" + templatePath
-        + "] and lang [" + lang + "] and defLang [" + defLang + "].");
+    LOGGER.debug("renderInheritableDocument: call renderTemplatePath for [{}] and lang [{}]"
+        + " and defLang [{}].", templatePath, lang, defLang);
     return renderCommand.renderTemplatePath(templatePath, lang, defLang);
   }
 
